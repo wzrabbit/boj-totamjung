75d42d8a44929397a706f77296b3faf0e2867378
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-23 17:37:59 +0900

    📦 chore: storybook이 커스텀 절대경로를 인식할 수 있도록 변경

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,5 +15,21 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@components': resolve(__dirname, '../src/components'),
+        '@constants': resolve(__dirname, '../src/constants'),
+        '@domains': resolve(__dirname, '../src/domains'),
+        '@hooks': resolve(__dirname, '../src/hooks'),
+        '@images': resolve(__dirname, '../src/images'),
+        '@types': resolve(__dirname, '../src/types'),
+        '@utils': resolve(__dirname, '../src/utils'),
+      };
+    }
+
+    return config;
+  },
 };
 export default config;

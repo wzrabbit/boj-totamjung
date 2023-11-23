8ee25e37186fbfad45496195d42017bbe0d8c1ae
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-23 17:36:16 +0900

    📦 chore: storybook 및 종속되는 babel 라이브러리 설치

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import { resolve } from 'path';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+export default config;

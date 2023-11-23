6325cadd35d6c09a5ce463cfd224fc1c7045d1b3
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-23 17:19:15 +0900

    📦 chore: webpack 및 TypeScript 설치 및 config 파일 작성, 핵심 로더 사용
    - HtmlWebpackPlugin
    - CopyWebpackPlugin

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    options: './src/options.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/options.html',
+      filename: 'options.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+};

4da1359d9278e2ba71f6f77dcf5daa335ad123d9
요술토끼 <curious.wzrabbit@gmail.com>
2023-12-09 23:10:53 +0900

    [CHORE] styled-components의 글로벌 스타일 설정 및 Pretendard 웹 폰트 추가 (#13)
    * 📦 chore: 글로벌 스타일을 저장할 styles 디렉토리를 절대경로에 추가
    
    * 📦 chore: GlobalStyles 생성 및 기본 스타일 추가
    
    * 📦 chore: theme 파일 추가 및 linter가 타입을 읽을 수 있도록 타입 정의
    
    * 📦 chore: 스토리북에서 GlobalStyle과 theme를 사용할 수 있도록 설정
    
    * 📦 chore: options 페이지에서 GlobalStyle과 theme를 사용할 수 있도록 설정
    
    * 📦 chore: storybook과 option 페이지에 Pretendard 웹 폰트 추가

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    
+    box-sizing: border-box;
+  }
+  
+  body,
+  input,
+  select,
+  textarea,
+  button {
+    font-family: 'Pretendard';
+  }
+  
+  a,
+  a:hover,
+  a:active,
+  a:visited {
+    text-decoration: none;
+  }
+  
+  button {
+    border: 0;
+    
+    cursor: pointer;
+  }
+  
+  button:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+export default GlobalStyle;

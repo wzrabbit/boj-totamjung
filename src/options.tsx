cbdd144630ac614599b613b6febcee78214c252f
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-09 23:05:55 +0900

    📦 chore: options 페이지에서 GlobalStyle과 theme를 사용할 수 있도록 설정

--- src/options.tsx
+++ src/options.tsx
@@ -1,8 +1,20 @@
 import { render } from 'react-dom';
+import { StrictMode } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@styles/theme';
+import GlobalStyle from '@styles/GlobalStyle';
 import Test from '@components/Test';
 
 const Options = () => {
   return <Test message="와 리액뜨!!" />;
 };
 
-render(<Options />, document.getElementById('root'));
+render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Options />
+    </ThemeProvider>
+  </StrictMode>,
+  document.getElementById('root'),
+);

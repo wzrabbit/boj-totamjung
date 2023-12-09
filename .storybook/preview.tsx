75327cfeabc08b9cf7508da0b9e030be0858a66c
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-09 23:05:26 +0900

    📦 chore: 스토리북에서 GlobalStyle과 theme를 사용할 수 있도록 설정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import { theme } from '../src/styles/theme';
+import React from 'react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story />
+      </ThemeProvider>
+    </>
+  ),
+];
+
+export default preview;

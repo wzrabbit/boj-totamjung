8b65b23ebd1651c1f6c9562cf0ddcd8975a0e330
WizardRabbit <curious.wzrabbit@gmail.com>
2024-02-19 21:27:15 +0900

    ✨ feat: AlgorithmInfos 타입 추가 및 전체 알고리즘 정보 추가

--- src/types/algorithm.ts
+++ src/types/algorithm.ts
@@ -2,3 +2,11 @@ export interface Algorithm {
   id: number;
   name: string;
 }
+
+export interface AlgorithmInfo {
+  id: number;
+  name: string;
+  englishName: string;
+  tag: string;
+  alias: string[];
+}

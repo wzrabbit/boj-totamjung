7564557019c5f3913a4a50aa9df762acc855e0cf
WizardRabbit <curious.wzrabbit@gmail.com>
2024-02-20 14:21:03 +0900

    ✨ feat: useAlgorithmPool 커스텀 훅 구현
    - 검색어, 체크되어 있는 알고리즘을 관리
    - 검색어가 바뀔 때마다 검색 결과를 반환
    - 원하는 알고리즘 분류 하나를 체크 설정/해제 하거나, 모든 알고리즘 분류를 체크 설정/해제할 수 있음

---
+++ src/hooks/algorithm/useAlgorithmPool.ts
@@ -0,0 +1,47 @@
+import { getSearchResults } from '@domains/algorithm/getSearchResults';
+import { ALGORITHMS_COUNT } from '@constants/algorithmInfos';
+import { useState } from 'react';
+
+const useAlgorithmPool = () => {
+  const [keyword, setKeyword] = useState('');
+  const [checkedIds, setCheckedIds] = useState<number[]>([]);
+
+  const toggleAlgorithm = (id: number) => {
+    if (checkedIds.includes(id)) {
+      const newCheckedIds = checkedIds.filter((checkedId) => checkedId !== id);
+      setCheckedIds(() => newCheckedIds);
+      return;
+    }
+
+    const newCheckedIds = [...checkedIds, id];
+    setCheckedIds(() => newCheckedIds);
+  };
+
+  const checkAllAlgorithms = () => {
+    if (confirm('모든 알고리즘 분류를 체크 설정할까요?')) {
+      setCheckedIds(() =>
+        Array.from({ length: ALGORITHMS_COUNT }).map((_, index) => index + 1),
+      );
+    }
+  };
+
+  const uncheckAllAlgorithms = () => {
+    if (confirm('모든 알고리즘 분류를 체크 해제할까요?')) {
+      setCheckedIds(() => []);
+    }
+  };
+
+  const items = getSearchResults(keyword);
+
+  return {
+    keyword,
+    items,
+    checkedIds,
+    setKeyword,
+    toggleAlgorithm,
+    checkAllAlgorithms,
+    uncheckAllAlgorithms,
+  };
+};
+
+export default useAlgorithmPool;

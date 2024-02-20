456e13fd387bf0bb141cf7a4adf91255b5e3d568
WizardRabbit <curious.wzrabbit@gmail.com>
2024-02-20 14:21:38 +0900

    ✨ feat: AlgorithmPool 컴포넌트에 커스텀 훅을 부착

--- src/components/AlgorithmPool/AlgorithmPool.tsx
+++ src/components/AlgorithmPool/AlgorithmPool.tsx
@@ -1,13 +1,32 @@
 import * as S from './AlgorithmPool.styled';
 import AlgorithmList from './AlgorithmList';
+import useAlgorithmPool from '@hooks/algorithm/useAlgorithmPool';
 
 const AlgorithmPool = () => {
+  const {
+    keyword,
+    items,
+    checkedIds,
+    setKeyword,
+    toggleAlgorithm,
+    checkAllAlgorithms,
+    uncheckAllAlgorithms,
+  } = useAlgorithmPool();
+
   return (
     <S.Container>
-      <AlgorithmList items={[]} checkedIds={[]} onChange={() => {}} />
-      <input />
-      <button>전체 선택</button>
-      <button>전체 해제</button>
+      <AlgorithmList
+        items={items}
+        checkedIds={checkedIds}
+        onChange={toggleAlgorithm}
+      />
+      <input
+        placeholder="검색어를 입력해 주세요..."
+        value={keyword}
+        onChange={(event) => setKeyword(() => event.target.value)}
+      />
+      <button onClick={checkAllAlgorithms}>전체 선택</button>
+      <button onClick={uncheckAllAlgorithms}>전체 해제</button>
     </S.Container>
   );
 };

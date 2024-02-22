1237cc916d26f9470abd9823e6c7e73b91504291
WizardRabbit <curious.wzrabbit@gmail.com>
2024-02-22 15:04:57 +0900

    ✨ refactor: AlgorithmList에서 여러 줄의 알고리즘 분류를 보여줄 때 위로 몰아서 보여주도록 설정

--- src/components/AlgorithmPool/AlgorithmList/AlgorithmList.styled.ts
+++ src/components/AlgorithmPool/AlgorithmList/AlgorithmList.styled.ts
@@ -2,6 +2,7 @@ import { styled } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  align-content: flex-start;
   flex-wrap: wrap;
   gap: 6px;
   overflow-y: auto;

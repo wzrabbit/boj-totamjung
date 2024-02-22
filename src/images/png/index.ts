5fe8ca23dce1a7a609a483d271ef567b95938238
WizardRabbit <curious.wzrabbit@gmail.com>
2024-02-22 15:04:09 +0900

    ✨ feat: 검색 아이콘, 전체 체크 활성화/비활성화 이미지 추가

---
+++ src/images/png/index.ts
@@ -0,0 +1,2 @@
+export { default as allCheckedIcon } from './all-checked.png';
+export { default as allUncheckedIcon } from './all-unchecked.png';
---
+++ src/images/svg/index.ts
@@ -0,0 +1,1 @@
+export { ReactComponent as SearchIcon } from './search.svg';

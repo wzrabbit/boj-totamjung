f471bc6e753e7ce1b0f8cc29587f099af8dd32bf
WizardRabbit <curious.wzrabbit@gmail.com>
2024-03-07 20:23:49 +0900

    ✨ feat: RandomDefenseHistoryList 컴포넌트 구현

---
+++ src/components/RandomDefenseHistoryMenu/RandomDefenseHistoryList/RandomDefenseHistoryList.styled.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 4px;
+  overflow-y: scroll;
+
+  width: 330px;
+  height: 100%;
+`;

befb0d8cf60324a5f72f4b3663266a47a46673e9
WizardRabbit <curious.wzrabbit@gmail.com>
2024-03-17 00:32:10 +0900

    ✨ feat: NamedFrame를 RandomDefenseHistoryMenu에 적용하고, 상단 티어 숨기기 메뉴를 제작

--- src/components/RandomDefenseHistoryMenu/RandomDefenseHistoryMenu.styled.ts
+++ src/components/RandomDefenseHistoryMenu/RandomDefenseHistoryMenu.styled.ts
@@ -1,3 +1,39 @@
 import { styled } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const HistoryListContainer = styled.div`
+  width: 348px;
+  height: 414px;
+  padding: 8px;
+
+  border: 1px solid ${({ theme }) => theme.color.LIGHT_BROWN};
+  border-radius: 6px;
+`;
+
+export const TierSwitchPanel = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+
+  margin-top: 12px;
+  height: 30px;
+`;
+
+export const DeletePanel = styled.div`
+  height: 36px;
+`;
+
+export const Text = styled.p`
+  display: inline-block;
+
+  color: ${({ theme }) => theme.color.WHITE};
+  font-size: 16px;
+`;
--- src/components/RandomDefenseHistoryMenu/RandomDefenseHistoryMenu.tsx
+++ src/components/RandomDefenseHistoryMenu/RandomDefenseHistoryMenu.tsx
@@ -1,25 +1,49 @@
 import RandomDefenseHistoryList from './RandomDefenseHistoryList';
 import useRandomDefenseHistoryMenu from '~hooks/randomDefense/useRandomDefenseHistoryMenu';
 import * as S from './RandomDefenseHistoryMenu.styled';
+import Switch from '~components/common/Switch';
+import NamedFrame from '~components/common/NamedFrame/NamedFrame';
 
 const RandomDefenseHistoryMenu = () => {
-  const { items, isHidden, deleteHistoryById, clearHistory, setIsHidden } =
-    useRandomDefenseHistoryMenu();
+  const {
+    items,
+    isHidden,
+    isLoaded,
+    deleteHistoryById,
+    clearHistory,
+    updateIsHidden,
+  } = useRandomDefenseHistoryMenu();
+
   return (
-    <S.Container>
-      <input
-        type="checkbox"
-        onChange={(e) => {
-          setIsHidden(e.target.checked);
-        }}
-      />
-      <RandomDefenseHistoryList
-        items={items}
-        isHidden={isHidden}
-        onDelete={deleteHistoryById}
-      />
-      <button onClick={clearHistory}>모두 삭제</button>
-    </S.Container>
+    <NamedFrame width="370px" height="520px" padding="10px" title="추첨 기록">
+      <S.Container>
+        {isLoaded && (
+          <>
+            <S.TierSwitchPanel>
+              <S.Text>티어 숨기기</S.Text>
+              <Switch
+                size="large"
+                isChecked={isHidden}
+                ariaLabel="티어 숨기기"
+                onChange={updateIsHidden}
+              />
+            </S.TierSwitchPanel>
+            <S.HistoryListContainer>
+              <RandomDefenseHistoryList
+                items={items}
+                isHidden={isHidden}
+                onDelete={deleteHistoryById}
+              />
+            </S.HistoryListContainer>
+            <S.DeletePanel>
+              <button onClick={clearHistory} disabled={items.length === 0}>
+                모두 삭제
+              </button>
+            </S.DeletePanel>
+          </>
+        )}
+      </S.Container>
+    </NamedFrame>
   );
 };
 

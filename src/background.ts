f824c32de9d043e3ac93447734e9b98b787aae27
요술토끼 <curious.wzrabbit@gmail.com>
2024-03-19 13:46:15 +0900

    ✨ feat: 서비스 워커에서 잘못된 메시지가 온 경우에 대한 로직을 구현

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,13 @@
+import { isObject } from '~types/typeGuards';
+
 chrome.runtime.onMessage.addListener(
-  (message: unknown, sender, sendResponse) => {},
+  (message: unknown, sender, sendResponse) => {
+    if (
+      !isObject(message) ||
+      !('command' in message) ||
+      typeof message.command !== 'string'
+    ) {
+      throw Error('잘못된 메시지가 전달되었습니다.');
+    }
+  },
 );

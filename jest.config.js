0c4f3b09511952cf9d7d3cbcf1195b53c41d571f
요술토끼 <curious.wzrabbit@gmail.com>
2024-03-26 13:13:30 +0900

    [CHORE] Jest의 테스트에서 svg 파일을 변형하여 사용할 수 있도록 설정 진행 (#23)
    * 📦 chore: jest-svg-transformer 설치
    
    * 📦 chore: 매핑할 절대경로 주소 추가, path alias 변경
    
    * 📦 chore: svg 파일을 매핑할 수 있도록 설정 진행

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@((?:components|utils|hooks|constants|domains).*)$': '<rootDir>/src/$1',
+    '^~((?:components|utils|hooks|constants|domains|types|images).*)$':
+      '<rootDir>/src/$1',
+    '^.+\\.svg$': 'jest-svg-transformer',
   },
 };

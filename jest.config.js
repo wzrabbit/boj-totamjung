751ec23ec9602f2bd422d5d77bfed49c2f3b64ee
요술토끼 <curious.wzrabbit@gmail.com>
2024-03-26 13:03:51 +0900

    📦 chore: svg 파일을 매핑할 수 있도록 설정 진행

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   moduleNameMapper: {
     '^~((?:components|utils|hooks|constants|domains|types|images).*)$':
       '<rootDir>/src/$1',
+    '^.+\\.svg$': 'jest-svg-transformer',
   },
 };

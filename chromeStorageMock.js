2fd2a2a47fbcea78cbd41dadbbbf0ab5a30802d5
WizardRabbit <curious.wzrabbit@gmail.com>
2024-04-05 23:08:54 +0900

    🛠 fix: setup 파일의 잘못된 오류 메시지를 고치고, sendMessage에 대한 모킹을 추가

--- chromeStorageMock.js
+++ chromeStorageMock.js
@@ -8,7 +8,7 @@ global.chrome = {
       },
       set: async () => {
         throw Error(
-          'chrome.storage.sync.get에 대한 모킹이 구현되지 않았습니다.',
+          'chrome.storage.sync.set에 대한 모킹이 구현되지 않았습니다.',
         );
       },
     },
@@ -16,14 +16,22 @@ global.chrome = {
     local: {
       get: async () => {
         throw Error(
-          'chrome.storage.sync.get에 대한 모킹이 구현되지 않았습니다.',
+          'chrome.storage.local.get에 대한 모킹이 구현되지 않았습니다.',
         );
       },
       set: async () => {
         throw Error(
-          'chrome.storage.sync.get에 대한 모킹이 구현되지 않았습니다.',
+          'chrome.storage.local.set에 대한 모킹이 구현되지 않았습니다.',
         );
       },
+    },
+  },
+
+  runtime: {
+    sendMessage: async () => {
+      throw Error(
+        'chrome.runtime.sendMessage에 대한 모킹이 구현되지 않았습니다.',
+      );
     },
   },
 };

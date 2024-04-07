1eaf3ceb9087e195a60e42d1a371478654f6ee16
WizardRabbit <curious.wzrabbit@gmail.com>
2024-04-07 19:27:37 +0900

    ✨ feat: DifficultyAdjustMenu 컴포넌트 구현

---
+++ src/components/DifficultyAdjustMenu/DifficultyAdjustMenu.tsx
@@ -0,0 +1,27 @@
+import * as S from './DifficultyAdjustMenu.styled';
+import type { TierWithoutNotRatable } from '~types/randomDefense';
+import TierSlider from './TierSlider';
+import TierRange from './TierRange';
+import TierPresetButtonList from './TierPresetButtonList';
+
+interface DifficultyAdjustMenuProps {
+  startTier: TierWithoutNotRatable;
+  endTier: TierWithoutNotRatable;
+  onChange: (start: TierWithoutNotRatable, end: TierWithoutNotRatable) => void;
+}
+
+const DifficultyAdjustMenu = (props: DifficultyAdjustMenuProps) => {
+  const { startTier, endTier, onChange } = props;
+
+  return (
+    <S.Container>
+      <TierSlider startTier={startTier} endTier={endTier} onChange={onChange} />
+      <S.PresetPanel>
+        <TierRange startTier={startTier} endTier={endTier} />
+        <TierPresetButtonList onClick={onChange} />
+      </S.PresetPanel>
+    </S.Container>
+  );
+};
+
+export default DifficultyAdjustMenu;

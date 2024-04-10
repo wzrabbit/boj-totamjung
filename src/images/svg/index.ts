01f59f8fdf6c76dbe8eb56a19749d55ef7405edb
WizardRabbit <curious.wzrabbit@gmail.com>
2024-04-10 23:07:34 +0900

    📦 chore: 링크 svg 파일 추가

--- src/images/svg/index.ts
+++ src/images/svg/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as ClockIcon } from './clock.svg';
 export { ReactComponent as TrashIcon } from './trash.svg';
 export { ReactComponent as PackageIcon } from './package.svg';
+export { ReactComponent as LinkIcon } from './link.svg';

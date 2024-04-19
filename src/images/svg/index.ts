1c3e8a6e2a0a955a1c2dafb5f6bf59c94c3dcf44
WizardRabbit <curious.wzrabbit@gmail.com>
2024-04-20 00:01:10 +0900

    📦 chore: 닫기 svg 아이콘 추가

--- src/images/svg/index.ts
+++ src/images/svg/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as ClockIcon } from './clock.svg';
 export { ReactComponent as TrashIcon } from './trash.svg';
 export { ReactComponent as PackageIcon } from './package.svg';
 export { ReactComponent as LinkIcon } from './link.svg';
+export { ReactComponent as CloseIcon } from './close.svg';

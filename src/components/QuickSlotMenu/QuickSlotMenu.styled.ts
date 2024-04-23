0bb44135e487bfcd02b524734e01226b80e70097
요술토끼 <curious.wzrabbit@gmail.com>
2024-04-23 14:24:03 +0900

    ✨ feat: QuickSlotMenu 컴포넌트 구현

---
+++ src/components/QuickSlotMenu/QuickSlotMenu.styled.ts
@@ -0,0 +1,25 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const SlotNumberPanel = styled.div`
+  display: flex;
+  column-gap: 10px;
+
+  height: 26px;
+`;
+
+export const SlotControlPanel = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  column-gap: 8px;
+
+  height: 32px;
+`;
---
+++ src/components/QuickSlotMenu/QuickSlotMenu.tsx
@@ -0,0 +1,67 @@
+import * as S from './QuickSlotMenu.styled';
+import NamedFrame from '~components/common/NamedFrame';
+import SlotPagination from './SlotPagination';
+import SlotInfo from './SlotInfo';
+import HotkeySwitcher from './HotkeySwitcher';
+import IconButton from '~components/common/IconButton';
+import useQuickSlotMenu from '~hooks/randomDefense/useQuickSlotMenu';
+import { CopyIcon, EditIcon, TrashIcon } from '~images/svg';
+import { theme } from '~styles/theme';
+
+const QuickSlotMenu = () => {
+  const { selectedSlotNumber, hotkey, updateSelectedSlotNumber, switchHotkey } =
+    useQuickSlotMenu();
+  return (
+    <NamedFrame width="650px" height="176px" padding="10px" title="퀵 슬롯">
+      <S.Container>
+        <S.SlotNumberPanel>
+          <SlotPagination
+            selectedSlotNumber={selectedSlotNumber}
+            occupiedSlotNumbers={[2, 4, 8]}
+            onChange={updateSelectedSlotNumber}
+          />
+          <HotkeySwitcher
+            selectedSlotNumber={selectedSlotNumber}
+            hotkey={hotkey}
+            onClick={switchHotkey}
+          />
+        </S.SlotNumberPanel>
+        <SlotInfo isEmpty={true} />
+        <S.SlotControlPanel>
+          <IconButton
+            name="쿼리 복사"
+            size="medium"
+            width="98px"
+            color={theme.color.LIGHT_GRAY}
+            iconSrc={<CopyIcon />}
+            disabled={false}
+            ariaLabel="쿼리 클립보드에 복사하기"
+            onClick={() => {}}
+          />
+          <IconButton
+            name="수정"
+            size="medium"
+            width="72px"
+            color={theme.color.SKY_BLUE}
+            iconSrc={<EditIcon />}
+            disabled={false}
+            ariaLabel="쿼리 수정하기"
+            onClick={() => {}}
+          />
+          <IconButton
+            name="삭제"
+            size="medium"
+            width="72px"
+            color={theme.color.RED}
+            iconSrc={<TrashIcon />}
+            disabled={false}
+            ariaLabel="쿼리 삭제하기"
+            onClick={() => {}}
+          />
+        </S.SlotControlPanel>
+      </S.Container>
+    </NamedFrame>
+  );
+};
+
+export default QuickSlotMenu;

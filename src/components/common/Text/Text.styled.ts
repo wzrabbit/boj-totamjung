bacffc4f022009d8dc5138ce06e259e3f1e374d2
요술토끼 <curious.wzrabbit@gmail.com>
2024-04-23 15:20:52 +0900

    [FEAT] 텍스트 공통 컴포넌트에서 사용 가능한 폰트 사이즈의 선택지를 늘리고, code 옵션을 추가 (#35)
    * ✨ feat: 코드 형태의 폰트를 사용하기 위해 options.html와 스토리북에 IBM Plex Mono 폰트 추가
    
    * ✨ feat: Text 공통 컴포넌트에서 사이즈 옵션을 추가하고, code 타입의 텍스트를 사용할 수 있도록 변경
    
    * 🧪 test: Text 컴포넌트에 대한 스토리북에서 더 다양한 사용 예시에 해당하는 스토리들을 추가

--- src/components/common/Text/Text.styled.ts
+++ src/components/common/Text/Text.styled.ts
@@ -1,11 +1,23 @@
 import { styled, css } from 'styled-components';
 
-export const Text = styled.p<{ $type: 'primary' | 'normal' }>`
+export const Text = styled.p<{
+  $type: 'primary' | 'normal' | 'code';
+  $fontSize: '16px' | '14px' | '13px';
+}>`
+  font-size: ${({ $fontSize }) => $fontSize};
+
   ${({ theme, $type }) => {
     if ($type === 'primary') {
       return css`
         color: ${theme.color.GOLD};
         font-weight: 600;
+      `;
+    }
+
+    if ($type === 'code') {
+      return css`
+        font-family: 'IBM Plex Mono', Consolas, Pretendard;
+        color: ${theme.color.WHITE};
       `;
     }
 

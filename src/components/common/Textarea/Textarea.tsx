c79fc3c1c4c7a3202ab1c0f9db6420b3f051dbbd
요술토끼 <curious.wzrabbit@gmail.com>
2024-04-29 09:58:43 +0900

    ✨ feat: 공통 Textarea 컴포넌트 구현

---
+++ src/components/common/Textarea/Textarea.tsx
@@ -0,0 +1,29 @@
+import * as S from './Textarea.styled';
+
+interface TextareaProps {
+  width: number;
+  height: number;
+  value: string;
+  minLength?: number;
+  maxLength?: number;
+  placeholder: string;
+  hasError: boolean;
+  ariaLabel: string;
+  onChange: () => void;
+}
+
+const Textarea = (props: TextareaProps) => {
+  const { width, height, hasError, ariaLabel, ...rest } = props;
+
+  return (
+    <S.Textarea
+      $width={width}
+      $height={height}
+      $hasError={hasError}
+      aria-label={ariaLabel}
+      {...rest}
+    />
+  );
+};
+
+export default Textarea;

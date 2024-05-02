965ef97c56c194c3df2b0969906cc1f819916cbb
WizardRabbit <curious.wzrabbit@gmail.com>
2024-05-02 21:01:55 +0900

    ✨ feat: 퀵슬롯을 불러오는 기본 로직을 구현하고, 현재 슬롯에 해당하는 정보를 반환하도록 기능 추가

--- src/hooks/randomDefense/useQuickSlotMenu.ts
+++ src/hooks/randomDefense/useQuickSlotMenu.ts
@@ -1,21 +1,76 @@
-import { useState } from 'react';
-import type { Hotkey } from '~types/randomDefense';
+import { useState, useEffect } from 'react';
+import { isQuickSlotsResponse } from '~types/typeGuards';
+import { COMMANDS } from '~constants/commands';
+import type { QuickSlots, SlotNo, Hotkey } from '~types/randomDefense';
+
+const emptySlots: QuickSlots = {
+  1: { isEmpty: true },
+  2: { isEmpty: true },
+  3: { isEmpty: true },
+  4: { isEmpty: true },
+  5: { isEmpty: true },
+  6: { isEmpty: true },
+  7: { isEmpty: true },
+  8: { isEmpty: true },
+  9: { isEmpty: true },
+  0: { isEmpty: true },
+};
+
+const SLOT_NOS: SlotNo[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 const useQuickSlotMenu = () => {
-  const [selectedSlotNo, setSelectedSlotNo] = useState(1);
+  const [slots, setSlots] = useState<QuickSlots>(emptySlots);
+  const [selectedSlotNo, setSelectedSlotNo] = useState<SlotNo>(1);
   const [hotkey, setHotkey] = useState<Hotkey>('Alt');
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  const updateSelectedSlotNo = (newSlotNo: number) => {
-    setSelectedSlotNo(() => newSlotNo);
+  useEffect(() => {
+    const fetchQuickSlots = async () => {
+      const response = await chrome.runtime.sendMessage({
+        command: COMMANDS.FETCH_QUICK_SLOTS,
+      });
+
+      if (!isQuickSlotsResponse(response)) {
+        return;
+      }
+
+      setSlots(response.slots);
+      setSelectedSlotNo(response.selectedSlotNo);
+      setHotkey(response.hotkey);
+      setIsLoaded(true);
+    };
+
+    fetchQuickSlots();
+  }, []);
+
+  const updateSelectedSlotNo = (newSlotNo: SlotNo) => {
+    setSelectedSlotNo(newSlotNo);
   };
 
   const switchHotkey = () => {
     setHotkey((prev) => (prev === 'Alt' ? 'F2' : 'Alt'));
   };
 
+  const getOccupiedSlotNos = () => {
+    const occupiedSlotNos: SlotNo[] = [];
+
+    SLOT_NOS.forEach((slotNo) => {
+      if (!slots[slotNo].isEmpty) {
+        occupiedSlotNos.push(slotNo);
+      }
+    });
+
+    return occupiedSlotNos;
+  };
+
+  const slot = slots[selectedSlotNo];
+
   return {
+    slot,
     selectedSlotNo,
+    occupiedSlotNos: getOccupiedSlotNos(),
     hotkey,
+    isLoaded,
     updateSelectedSlotNo,
     switchHotkey,
   };

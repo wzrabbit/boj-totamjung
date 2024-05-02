924022daf76d6cacb01b50964cb9c83d95e1ec88
WizardRabbit <curious.wzrabbit@gmail.com>
2024-05-02 21:02:35 +0900

    ✨ feat: useQuickSlotMenu에서 구현된 내용을 반영 및 클립보드에 쿼리를 복사하는 기능 구현

--- src/components/QuickSlotMenu/QuickSlotMenu.tsx
+++ src/components/QuickSlotMenu/QuickSlotMenu.tsx
@@ -9,58 +9,72 @@ import { CopyIcon, EditIcon, TrashIcon } from '~images/svg';
 import { theme } from '~styles/theme';
 
 const QuickSlotMenu = () => {
-  const { selectedSlotNo, hotkey, updateSelectedSlotNo, switchHotkey } =
-    useQuickSlotMenu();
+  const {
+    slot,
+    selectedSlotNo,
+    occupiedSlotNos,
+    hotkey,
+    isLoaded,
+    updateSelectedSlotNo,
+    switchHotkey,
+  } = useQuickSlotMenu();
 
   return (
     <NamedFrame width="650px" height="176px" padding="10px" title="퀵 슬롯">
-      <S.Container>
-        <S.SlotNoPanel>
-          <SlotPagination
-            selectedSlotNo={selectedSlotNo}
-            occupiedSlotNos={[2, 4, 8]}
-            onChange={updateSelectedSlotNo}
-          />
-          <HotkeySwitcher
-            selectedSlotNo={selectedSlotNo}
-            hotkey={hotkey}
-            onClick={switchHotkey}
-          />
-        </S.SlotNoPanel>
-        <SlotInfo isEmpty={true} />
-        <S.SlotControlPanel>
-          <IconButton
-            name="쿼리 복사"
-            size="medium"
-            width="98px"
-            color={theme.color.LIGHT_GRAY}
-            iconSrc={<CopyIcon />}
-            disabled={false}
-            ariaLabel="쿼리 클립보드에 복사하기"
-            onClick={() => {}}
-          />
-          <IconButton
-            name="수정"
-            size="medium"
-            width="72px"
-            color={theme.color.SKY_BLUE}
-            iconSrc={<EditIcon />}
-            disabled={false}
-            ariaLabel="쿼리 수정하기"
-            onClick={() => {}}
-          />
-          <IconButton
-            name="삭제"
-            size="medium"
-            width="72px"
-            color={theme.color.RED}
-            iconSrc={<TrashIcon />}
-            disabled={false}
-            ariaLabel="쿼리 삭제하기"
-            onClick={() => {}}
-          />
-        </S.SlotControlPanel>
-      </S.Container>
+      {isLoaded && (
+        <S.Container>
+          <S.SlotNoPanel>
+            <SlotPagination
+              selectedSlotNo={selectedSlotNo}
+              occupiedSlotNos={occupiedSlotNos}
+              onChange={updateSelectedSlotNo}
+            />
+            <HotkeySwitcher
+              selectedSlotNo={selectedSlotNo}
+              hotkey={hotkey}
+              onClick={switchHotkey}
+            />
+          </S.SlotNoPanel>
+          <SlotInfo {...slot} />
+          <S.SlotControlPanel>
+            <IconButton
+              name="쿼리 복사"
+              size="medium"
+              width="98px"
+              color={theme.color.LIGHT_GRAY}
+              iconSrc={<CopyIcon />}
+              disabled={slot.isEmpty}
+              ariaLabel="쿼리 클립보드에 복사하기"
+              onClick={() => {
+                if (!slot.isEmpty) {
+                  navigator.clipboard.writeText(slot.query);
+                  alert('쿼리를 복사했어요!');
+                }
+              }}
+            />
+            <IconButton
+              name="수정"
+              size="medium"
+              width="72px"
+              color={theme.color.SKY_BLUE}
+              iconSrc={<EditIcon />}
+              disabled={false}
+              ariaLabel="쿼리 수정하기"
+              onClick={() => {}}
+            />
+            <IconButton
+              name="삭제"
+              size="medium"
+              width="72px"
+              color={theme.color.RED}
+              iconSrc={<TrashIcon />}
+              disabled={false}
+              ariaLabel="쿼리 삭제하기"
+              onClick={() => {}}
+            />
+          </S.SlotControlPanel>
+        </S.Container>
+      )}
     </NamedFrame>
   );
 };

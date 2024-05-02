48037e15593088316b881da03ec85fcb6a43c56d
WizardRabbit <curious.wzrabbit@gmail.com>
2024-05-02 21:00:44 +0900

    ✨ feat: 퀵슬롯 관련 스토리지 키 상수 추가

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -3,10 +3,13 @@ export const COMMANDS = {
   SAVE_CHECKED_ALGORITHM_IDS: 'saveCheckedAlgorithmIds',
   FETCH_RANDOM_DEFENSE_HISTORY: 'fetchRandomDefenseHistory',
   SAVE_RANDOM_DEFENSE_HISTORY: 'saveRandomDefenseHistory',
+  FETCH_QUICK_SLOTS: 'fetchQuickSlots',
+  SAVE_QUICK_SLOTS: 'saveQuickSlots',
 } as const;
 
 export const SYNC_STORAGE_KEY = {
   CHECKED_ALGORITHM_IDS: 'checkedAlgorithmIds',
+  QUICK_SLOTS: 'quickSlots',
 } as const;
 
 export const LOCAL_STORAGE_KEY = {
@@ -21,6 +24,7 @@ export const LOCAL_STORAGE_KEY = {
  */
 export const LEGACY_SYNC_STORAGE_KEY = {
   CHECKED_ALGORITHM_IDS: 'algorithm',
+  QUICK_SLOTS: 'query',
 } as const;
 
 export const LEGACY_LOCAL_STORAGE_KEY = {

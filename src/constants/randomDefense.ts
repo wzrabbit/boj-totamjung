789b10ff861546793808d8f246bbe242194826c0
WizardRabbit <curious.wzrabbit@gmail.com>
2024-05-13 22:23:14 +0900

    ✨ feat: 추첨 생성 폼 데이터 검증에 필요한 상수 추가

--- src/constants/randomDefense.ts
+++ src/constants/randomDefense.ts
@@ -11,3 +11,13 @@ export const MAX_PROBLEM_NAME_LENGTH = 500;
 export const MAX_HISTORY_LIMIT = 300;
 
 export const MAX_SEARCH_ALGORITHM_COUNT = 5;
+
+export const TITLE_MAX_LENGTH = 30;
+
+export const HANDLE_REGEX = /^\w{3,20}$/;
+
+export const MAX_SOLVED_COUNT = 100_000_000;
+
+export const NUMBER_REGEX = /^([1-9][0-9]*|0)$/;
+
+export const MAX_CUSTOM_QUERY_LENGTH = 300;

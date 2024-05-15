309075db6b07c4cb743fc04b8d1d637cb15655d6
WizardRabbit <curious.wzrabbit@gmail.com>
2024-05-15 20:46:37 +0900

    ✨ feat: Input과 Textarea의 spellcheck를 비활성화

--- src/components/common/Textarea/Textarea.tsx
+++ src/components/common/Textarea/Textarea.tsx
@@ -24,6 +24,7 @@ const Textarea = forwardRef((props: TextareaProps, ref) => {
       $width={width}
       $height={height}
       $hasError={hasError}
+      spellCheck={false}
       aria-label={ariaLabel}
       ref={ref}
       {...rest}

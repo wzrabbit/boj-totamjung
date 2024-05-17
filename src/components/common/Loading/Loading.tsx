f1e24d0925f3e312f3ac5fadeb389f5097389737
요술토끼 <curious.wzrabbit@gmail.com>
2024-05-17 09:08:53 +0900

    ✨ feat: Loading 공통 컴포넌트 구현

---
+++ src/components/common/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import { LoadingIcon } from '~images/svg';
+import * as S from './Loading.styled';
+
+const Loading = () => {
+  return (
+    <S.Container>
+      <LoadingIcon />
+    </S.Container>
+  );
+};
+
+export default Loading;

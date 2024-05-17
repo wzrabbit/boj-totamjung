89058c65cab009c83225f03c76afccb72f340d4e
WizardRabbit <curious.wzrabbit@gmail.com>
2024-05-17 21:14:32 +0900

    ✨ feat: RandomDefenseManageMenu 컴포넌트 구현

---
+++ src/components/RandomDefenseManageMenu/RandomDefenseManageMenu.styled.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+
+  width: 650px;
+  height: auto;
+`;

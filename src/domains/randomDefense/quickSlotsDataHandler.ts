c3198b4f672147320a56577300f4b882a3a58225
요술토끼 <curious.wzrabbit@gmail.com>
2024-05-29 11:59:12 +0900

    ✨ feat: quickSlotsDataHandler에 데이터를 가공/복구하는 함수, 구버전 데이터 형식을 최신 버전의 데이터 형식으로 변환시키는 함수 추가

--- src/domains/randomDefense/quickSlotsDataHandler.ts
+++ src/domains/randomDefense/quickSlotsDataHandler.ts
@@ -43,3 +43,99 @@ const EMPTY_VALID_LEGACY_QUICK_SLOTS_RESPONSE: LegacyQuickSlotsResponse = {
 };
 
 const SLOT_NOS: SlotNo[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+const sanitizeSlot = (slot: unknown, slotNo: SlotNo): Slot => {
+  if (!isSlot(slot)) {
+    return { isEmpty: true };
+  }
+
+  if (
+    !slot.isEmpty &&
+    (slot.query.trim() === '' || slot.query.length > MAX_CUSTOM_QUERY_LENGTH)
+  ) {
+    return { isEmpty: true };
+  }
+
+  if (
+    !slot.isEmpty &&
+    (slot.title.trim() === '' || slot.title.length > TITLE_MAX_LENGTH)
+  ) {
+    return {
+      ...slot,
+      isEmpty: false,
+      title: `추첨 ${slotNo}`,
+    };
+  }
+
+  return slot;
+};
+
+const sanitizeQuickSlots = (quickSlots: unknown): QuickSlotsResponse => {
+  if (!isRepairableQuickSlotsResponse(quickSlots)) {
+    return EMPTY_VALID_QUICK_SLOTS_RESPONSE;
+  }
+
+  const hotkey =
+    'hotkey' in quickSlots && isHotkey(quickSlots.hotkey)
+      ? quickSlots.hotkey
+      : 'Alt';
+  const selectedSlotNo =
+    'selectedSlotNo' in quickSlots && isSlotNo(quickSlots.selectedSlotNo)
+      ? quickSlots.selectedSlotNo
+      : 1;
+
+  const sanitizedQuickSlots = {
+    ...quickSlots,
+    hotkey,
+    selectedSlotNo,
+  };
+
+  SLOT_NOS.forEach((slotNo) => {
+    const slot = sanitizedQuickSlots.slots[slotNo];
+    sanitizedQuickSlots.slots[slotNo] = sanitizeSlot(slot, slotNo);
+  });
+
+  return isQuickSlotsResponse(sanitizedQuickSlots)
+    ? sanitizedQuickSlots
+    : EMPTY_VALID_QUICK_SLOTS_RESPONSE;
+};
+
+const sanitizeLegacyQuickSlots = (
+  legacyQuickSlots: unknown,
+): LegacyQuickSlotsResponse => {
+  if (!isRepairableLegacyQuickSlotsResponse(legacyQuickSlots)) {
+    return EMPTY_VALID_LEGACY_QUICK_SLOTS_RESPONSE;
+  }
+
+  const { selectedNo, ...slots } = legacyQuickSlots;
+
+  const sanitizedSlotNo = isSlotNo(selectedNo)
+    ? legacyQuickSlots.selectedNo
+    : 1;
+
+  const sanitizedLegacyQuickSlots = {
+    ...slots,
+    selectedNo: sanitizedSlotNo,
+  };
+
+  SLOT_NOS.forEach((slotNo) => {
+    const slot = sanitizedLegacyQuickSlots[slotNo];
+    sanitizedLegacyQuickSlots[slotNo] = sanitizeSlot(slot, slotNo);
+  });
+
+  return isLegacyQuickSlotsResponse(sanitizedLegacyQuickSlots)
+    ? sanitizedLegacyQuickSlots
+    : EMPTY_VALID_LEGACY_QUICK_SLOTS_RESPONSE;
+};
+
+const convertLegacyToLatestQuickSlots = (
+  legacyQuickSlots: LegacyQuickSlotsResponse,
+): QuickSlotsResponse => {
+  const { selectedNo, ...legacyQuickSlotsWithoutSelectedNo } = legacyQuickSlots;
+
+  return {
+    selectedSlotNo: selectedNo,
+    slots: legacyQuickSlotsWithoutSelectedNo,
+    hotkey: 'Alt',
+  };
+};

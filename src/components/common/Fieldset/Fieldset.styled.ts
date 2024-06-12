9345518a17f9eae3373ea47ab19bf9ba09369a09
요술토끼 <curious.wzrabbit@gmail.com>
2024-06-12 15:00:35 +0900

    ✨ feat: Fieldset 컴포넌트를 구현

---
+++ src/components/common/Fieldset/Fieldset.styled.ts
@@ -0,0 +1,32 @@
+import { styled } from 'styled-components';
+
+export const Fieldset = styled.fieldset`
+  width: 100%;
+  height: 100%;
+
+  user-select: none;
+
+  &:disabled {
+    opacity: 0.6;
+  }
+`;
+
+export const Legend = styled.legend`
+  color: ${({ theme }) => theme.color.WHITE};
+  margin-bottom: 6px;
+`;
+
+export const OptionsContainer = styled.div<{ $isVertical: boolean }>`
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  flex-direction: ${({ $isVertical }) => ($isVertical ? 'column' : 'row')};
+  row-gap: 6px;
+  column-gap: 10px;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+`;

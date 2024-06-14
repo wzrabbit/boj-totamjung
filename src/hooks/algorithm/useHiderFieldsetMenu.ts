92c7a7eb8e460e26415a6283b100d82d0309d45e
WizardRabbit <curious.wzrabbit@gmail.com>
2024-06-15 00:32:54 +0900

    ✨ feat: useHiderFieldsetMenu 커스텀 훅 구현
    - 로컬에서의 데이터 변경 기능 지원
    - 서비스워커에게 데이터 저장을 요청하는 기능 포함
    - 서비스워커로부터 데이터를 불러오도록 요청하는 기능 포함

---
+++ src/hooks/algorithm/useHiderFieldsetMenu.ts
@@ -0,0 +1,163 @@
+import { useState, useEffect } from 'react';
+import { isHiderOptionsResponse } from '~types/typeGuards';
+import type { HiderOptionsResponse } from '~types/algorithm';
+import { COMMANDS } from '~constants/commands';
+import { RatedTier } from '~types/tierHider';
+
+const fallbackHiderOptions = {
+  problemTagLockDuration: {
+    hours: 0,
+    minutes: 0,
+  },
+  shouldHideTier: undefined,
+  shouldWarnHighTier: undefined,
+  warnTier: 1 as const,
+  algorithmHiderUsage: undefined,
+  problemTagLockUsage: undefined,
+};
+
+type HiderOptionsState =
+  | HiderOptionsReadyResponse
+  | HiderOptionsNotReadyResponse;
+
+type HiderOptionsReadyResponse = {
+  isLoaded: true;
+} & HiderOptionsResponse;
+
+type HiderOptionsNotReadyResponse = {
+  isLoaded: false;
+} & typeof fallbackHiderOptions;
+
+const useHiderFieldsetMenu = () => {
+  const [hiderOptionsState, setHiderOptionsState] = useState<HiderOptionsState>(
+    {
+      ...fallbackHiderOptions,
+      isLoaded: false,
+    },
+  );
+
+  useEffect(() => {
+    const fetchHiderOptions = async () => {
+      const response = await chrome.runtime.sendMessage({
+        command: COMMANDS.FETCH_HIDER_OPTIONS,
+      });
+
+      if (!isHiderOptionsResponse(response)) {
+        return;
+      }
+
+      setHiderOptionsState({
+        ...response,
+        isLoaded: true,
+      });
+    };
+
+    fetchHiderOptions();
+  }, []);
+
+  useEffect(() => {
+    const { isLoaded, ...hiderOptionsWithoutLoadingParam } = hiderOptionsState;
+
+    if (!isLoaded) {
+      return;
+    }
+
+    chrome.runtime.sendMessage({
+      command: COMMANDS.SAVE_HIDER_OPTIONS,
+      ...hiderOptionsWithoutLoadingParam,
+    });
+  }, [hiderOptionsState]);
+
+  const updateProblemTagLockDuration = (hours: number, minutes: number) => {
+    setHiderOptionsState((prev) => {
+      if (!prev.isLoaded) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        problemTagLockDuration: {
+          hours,
+          minutes,
+        },
+      };
+    });
+  };
+
+  const updateShouldHideTier = (shouldHideTierString: string) => {
+    const shouldHideTier = shouldHideTierString === 'true';
+
+    setHiderOptionsState((prev) => {
+      if (!prev.isLoaded) {
+        return prev;
+      }
+
+      return { ...prev, shouldHideTier };
+    });
+  };
+
+  const updateShouldWarnHighTier = (shouldWarnHighTierString: string) => {
+    const shouldWarnHighTier = shouldWarnHighTierString === 'true';
+
+    setHiderOptionsState((prev) => {
+      if (!prev.isLoaded) {
+        return prev;
+      }
+
+      return { ...prev, shouldWarnHighTier };
+    });
+  };
+
+  const updateWarnTier = (warnTier: RatedTier) => {
+    setHiderOptionsState((prev) => {
+      if (!prev.isLoaded) {
+        return prev;
+      }
+
+      return { ...prev, warnTier };
+    });
+  };
+
+  const updateAlgorithmHiderUsage = (usage: string) => {
+    if (usage !== 'click' && usage !== 'auto') {
+      return;
+    }
+
+    setHiderOptionsState((prev) => {
+      if (!prev.isLoaded) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        algorithmHiderUsage: usage,
+      };
+    });
+  };
+
+  const updateProblemTagLockUsage = (usage: string) => {
+    if (usage !== 'click' && usage !== 'auto') {
+      return;
+    }
+
+    setHiderOptionsState((prev) => {
+      if (!prev.isLoaded) {
+        return prev;
+      }
+
+      return { ...prev, problemTagLockUsage: usage };
+    });
+  };
+
+  return {
+    ...hiderOptionsState,
+    updateProblemTagLockDuration,
+    updateShouldHideTier,
+    updateShouldWarnHighTier,
+    updateWarnTier,
+    updateAlgorithmHiderUsage,
+    updateProblemTagLockUsage,
+  };
+};
+
+export default useHiderFieldsetMenu;

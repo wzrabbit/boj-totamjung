88ba0c372496730587b1a9a1921078d6a6a1430d
WizardRabbit <curious.wzrabbit@gmail.com>
2024-07-07 01:16:22 +0900

    ♻️ refactor: 로직 분리를 위한 빈 줄 삽입

--- src/domains/dataHandlers/sanitizers/hiderOptionsSanitizer.ts
+++ src/domains/dataHandlers/sanitizers/hiderOptionsSanitizer.ts
@@ -1,5 +1,6 @@
 import { DEFAULT_HIDER_OPTIONS } from '~constants/defaultValues';
 import { isHiderOptionsResponse } from '../validators/hiderOptionsValidator';
+
 export const hiderOptionsSanitizer = (hiderOptions: unknown) => {
   return isHiderOptionsResponse(hiderOptions)
     ? hiderOptions

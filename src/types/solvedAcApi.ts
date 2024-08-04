a527aeff7734dd255b6c8954b1aa6db7d23dba52
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-05 01:09:45 +0900

    ✨ feat: solved.ac의 `/search/problem` 엔드포인트에 대응하는 API의 타입을 작성
    - 전체를 구현하지 않고, 토탐정에서 사용할 타입만을 구현하여 사용

---
+++ src/types/solvedAcApi.ts
@@ -0,0 +1,16 @@
+import { TierWithoutNotRatable } from '~types/randomDefense';
+
+/**
+ * `solved.ac/api/v3/search/problem`으로 solved.ac API에 쿼리 요청을 했을 경우 예상되는 정상 응답값입니다.
+ * 실제로 사용할 데이터만을 타입으로 포함했음에 유의하여 주십시오.
+ */
+export interface SolvedAcSearchProblemResponse {
+  count: number;
+  items: SolvedAcSearchProblemInfo[];
+}
+
+export interface SolvedAcSearchProblemInfo {
+  problemId: number;
+  titleKo: string;
+  level: TierWithoutNotRatable;
+}

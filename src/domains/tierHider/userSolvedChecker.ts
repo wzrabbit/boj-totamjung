aaa186842320a8ce6ef568b7ffa10ff5a0c86e28
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-05 01:12:54 +0900

    ✨ feat: solved.ac의 API를 사용해 유저가 특정 문제를 풀었는지 판단하는 비동기 함수 구현
    - 질문 게시판의 단일 게시글 페이지의 경우 BOJ의 요소를 통해 사용자가 해당 문제를 맞췄는지를 판별하는 것이 불가능(성공 레이블, 맞췄음을 의미하는 녹색 문제 글씨 등 단서가 일체 없음)
    - 따라서, 해당 케이스의 경우 solved.ac의 API 요청을 이용해 이를 판단
    - solved.ac API 요청 횟수는 제한되어 있으므로 한정된 상황에서만 사용하는 것이 좋음

---
+++ src/domains/tierHider/userSolvedChecker.ts
@@ -0,0 +1,28 @@
+import { isSolvedAcSearchProblemResponse } from '~domains/dataHandlers/validators/solvedAcSearchProblemResponseValidator';
+
+export const isUserSolvedProblem = async (
+  handle: string,
+  problemId: number,
+) => {
+  const query = `@${handle} id:${problemId}`;
+
+  try {
+    const response = await fetch(
+      `https://solved.ac/api/v3/search/problem?query=${query}`,
+    );
+
+    if (!response.ok) {
+      return 'unknown';
+    }
+
+    const responseJson = await response.json();
+
+    if (!isSolvedAcSearchProblemResponse(responseJson)) {
+      return 'unknown';
+    }
+
+    return responseJson.count === 1;
+  } catch (error) {
+    return 'unknown';
+  }
+};

76138270ca7edf8ef01961a4f0cd8cf87cd00d70
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-05 01:13:33 +0900

    ✨ feat: 사이트 내의 티어 뱃지를 경고 티어로 변환하는 함수 구현

--- src/domains/tierHider/normalToWarnTierChanger.ts
+++ src/domains/tierHider/normalToWarnTierChanger.ts
@@ -1,3 +1,9 @@
+import { $, $$ } from '~utils/querySelector';
+import { solvedAcNumericTierIcons, solvedAcRankIcons } from '~images/svg/tier';
+import { isTierWithoutNotRatable } from '~types/typeGuards';
+import type { RatedTier } from '~types/tierHider';
+import { COMMANDS } from '~constants/commands';
+
 const TIER_FROM_SRC_REGEX = /\d+(?=\.svg$)/;
 
 const TIER_FROM_CLASS_TEXT_REGEX = /\d+$/;
@@ -27,3 +33,115 @@ const TIER_TEXT_CSS_SELECTOR =
 
 const BOJ_EXTENDED_PROBLEM_LINK_BOX_CSS_SELECTOR =
   ".problem-link-style-box:not(.result-ac):not([data-tier='0'])";
+
+export const changeNormalToWarnTier = (warnTier: RatedTier) => {
+  changeBoardBlockquoteBadgeTier(warnTier);
+  changeRegularBadgeTier(warnTier);
+  changeTierText(warnTier);
+  changeBojExtendedProblemLinkBox(warnTier);
+};
+
+const changeBoardBlockquoteBadgeTier = async (warnTier: RatedTier) => {
+  const boardBlockquoteTierBadgeElement = $(
+    BOARD_BLOCKQUOTE_TIER_BADGE_CSS_SELECTOR,
+  );
+  const handleElement = $(HANDLE_CSS_SELECTOR);
+  const boardBlockquoteProblemTextElement = $(
+    BOARD_BLOCKQUOTE_PROBLEM_TEXT_SELECTOR,
+  );
+
+  if (
+    !(boardBlockquoteTierBadgeElement instanceof HTMLImageElement) ||
+    !handleElement ||
+    !boardBlockquoteProblemTextElement
+  ) {
+    return;
+  }
+
+  const matchedProblemId = boardBlockquoteProblemTextElement.innerText.match(
+    PROBLEM_ID_FROM_BOARD_BLOCKQUOTE_PROBLEM_TEXT_REGEX,
+  );
+  const matchedTier =
+    boardBlockquoteTierBadgeElement.src.match(TIER_FROM_SRC_REGEX);
+
+  if (!matchedProblemId || !matchedTier) {
+    return;
+  }
+
+  const handle = handleElement.innerText.trim();
+  const problemId = Number(matchedProblemId);
+  const tier = Number(matchedTier);
+
+  if (!isTierWithoutNotRatable(tier)) {
+    return;
+  }
+
+  const isUserSolved = await chrome.runtime.sendMessage({
+    command: COMMANDS.IS_USER_SOLVED_PROBLEM,
+    handle,
+    problemId,
+  });
+
+  if (isUserSolved === 'unknown') {
+    boardBlockquoteTierBadgeElement.remove();
+    return;
+  }
+
+  if (isUserSolved) {
+    boardBlockquoteTierBadgeElement.style.content = `url(${solvedAcNumericTierIcons[tier]})`;
+    return;
+  }
+
+  if (tier >= warnTier) {
+    boardBlockquoteTierBadgeElement.style.content = `url(${solvedAcRankIcons.warn})`;
+  }
+};
+
+const changeRegularBadgeTier = (warnTier: RatedTier) => {
+  const tierBadgeElements = [
+    ...TIER_BADGE_CSS_SELECTORS.map((selector) => $$(selector)).flat(),
+  ];
+
+  tierBadgeElements.forEach((tierBadgeElement) => {
+    if (tierBadgeElement instanceof HTMLImageElement) {
+      const src = tierBadgeElement.src;
+      const tier = src.match(TIER_FROM_SRC_REGEX);
+
+      if (tier && Number(tier) >= warnTier) {
+        tierBadgeElement.style.content = `url(${solvedAcRankIcons.warn})`;
+      }
+    }
+  });
+};
+
+const changeTierText = (warnTier: RatedTier) => {
+  const tierTextElement = $(TIER_TEXT_CSS_SELECTOR);
+
+  if (!tierTextElement) {
+    return;
+  }
+
+  const className = tierTextElement.classList[0];
+
+  if (!className) {
+    return;
+  }
+
+  const tier = className.match(TIER_FROM_CLASS_TEXT_REGEX);
+
+  if (tier && Number(tier) >= warnTier) {
+    tierTextElement.classList.add('warn');
+  }
+};
+
+const changeBojExtendedProblemLinkBox = (warnTier: RatedTier) => {
+  const problemLinkBoxElements = $$(BOJ_EXTENDED_PROBLEM_LINK_BOX_CSS_SELECTOR);
+
+  problemLinkBoxElements.forEach((problemLinkBoxElement) => {
+    const tier = problemLinkBoxElement.getAttribute('data-tier');
+
+    if (tier && Number(tier) >= warnTier) {
+      problemLinkBoxElement.classList.add('warn');
+    }
+  });
+};

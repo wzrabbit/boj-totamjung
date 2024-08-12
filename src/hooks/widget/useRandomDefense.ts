4c7117f072c3b38c3cea3e9d37e34c10d2d9e321
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-12 20:33:53 +0900

    ✨ feat: keyCode 대신 code를 사용하고, 무작위 추첨 기능이 작동하도록 구현
    - keyCode를 사용했던 이유는 일부 사용자에게서 올바른 키를 눌렀음에도 다른 문자가 쳐지는 현상으로 인해 인식이 안 되는 문제를 수정하기 위함이었고, code를 같이 사용해 물리적으로 눌리는 키가 무엇인지를 감지하여 문제를 다른 방법으로 해결하고자 함
    - 키보드로 누르는 경우, 위젯을 클릭하는 경우를 대응
    - 무작위 추첨 관련 정보가 변경될 경우 자동 업데이트

--- src/hooks/widget/useRandomDefense.ts
+++ src/hooks/widget/useRandomDefense.ts
@@ -1,46 +1,211 @@
 import { useState, useEffect, useRef } from 'react';
+import { COMMANDS } from '~constants/commands';
 import { DEFAULT_QUICK_SLOTS_RESPONSE } from '~constants/defaultValues';
-import type { QuickSlotsResponse } from '~types/randomDefense';
+import {
+  isQuickSlotsResponse,
+  isSlotNo,
+} from '~domains/dataHandlers/validators/quickSlotsValidator';
+import type { ToastInfo } from '~types/toast';
+import type { QuickSlotsResponse, SlotNo } from '~types/randomDefense';
+import { isRandomDefenseResultResponse } from '~domains/dataHandlers/validators/RandomDefenseResultResponseValidator';
 
-const useRandomDefense = () => {
-  const [quickSlotsResponse, setQuickSlotsResponse] =
-    useState<QuickSlotsResponse>(DEFAULT_QUICK_SLOTS_RESPONSE);
+interface UseRandomDefenseParams {
+  onToast: (toastInfo: ToastInfo, duration: number) => void;
+}
+
+const useRandomDefense = (params: UseRandomDefenseParams) => {
+  const { onToast } = params;
+  const [isRandomDefenseAvailable, setIsRandomDefenseAvailable] =
+    useState(false);
+
+  const isRandomDefenseAvailableRef = useRef(isRandomDefenseAvailable);
   const pressedKeysRef = useRef<Set<string>>(new Set());
-  const pressedKeyCodesRef = useRef<Set<number>>(new Set());
-
-  /**
-   * keyCode는 deprecated이지만, 일부 사용자에게서 keyCode를 사용하지 않을 경우
-   * 단축키가 인식이 안 되는 문제가 있는 현상이 제보되어, 호환을 위해 유지합니다.
-   */
-  const addPressedKeyInfo = (event: globalThis.KeyboardEvent) => {
-    const key = event.key;
-    const keyCode = event.keyCode;
-
-    pressedKeysRef.current.add(key);
-    pressedKeyCodesRef.current.add(keyCode);
-  };
-
-  const deletePressedKeyInfo = (event: globalThis.KeyboardEvent) => {
-    const key = event.key;
-    const keyCode = event.keyCode;
-
-    pressedKeysRef.current.delete(key);
-    pressedKeyCodesRef.current.delete(keyCode);
-  };
+  const pressedCodesRef = useRef<Set<string>>(new Set());
+  const quickSlotsResponseRef = useRef<QuickSlotsResponse>(
+    DEFAULT_QUICK_SLOTS_RESPONSE,
+  );
 
   useEffect(() => {
+    const fetchQuickSlots = async () => {
+      const response = await chrome.runtime.sendMessage({
+        command: COMMANDS.FETCH_QUICK_SLOTS,
+      });
+
+      if (!isQuickSlotsResponse(response)) {
+        return;
+      }
+
+      quickSlotsResponseRef.current = response;
+      isRandomDefenseAvailableRef.current = true;
+      setIsRandomDefenseAvailable(true);
+    };
+
+    fetchQuickSlots();
     document.addEventListener('keydown', addPressedKeyInfo);
     document.addEventListener('keyup', deletePressedKeyInfo);
+    chrome.storage.onChanged.addListener(updateQuickSlotsIfLocalChanged);
 
     return () => {
       document.removeEventListener('keydown', addPressedKeyInfo);
       document.removeEventListener('keyup', deletePressedKeyInfo);
+      chrome.storage.onChanged.removeListener(updateQuickSlotsIfLocalChanged);
     };
   }, []);
 
+  const updateQuickSlotsIfLocalChanged = (
+    changes: { [key: string]: chrome.storage.StorageChange },
+    areaName: chrome.storage.AreaName,
+  ) => {
+    if (areaName !== 'local' || !('quickSlots' in changes)) {
+      return;
+    }
+
+    const { newValue } = changes.quickSlots;
+
+    if (!isQuickSlotsResponse(newValue)) {
+      return;
+    }
+
+    quickSlotsResponseRef.current = newValue;
+  };
+
+  const addPressedKeyInfo = (event: globalThis.KeyboardEvent) => {
+    const key = event.key;
+    const code = event.code;
+
+    pressedKeysRef.current.add(key);
+    pressedCodesRef.current.add(code);
+
+    const altPressed = event.altKey;
+    const f2Pressed = pressedCodesRef.current.has('F2');
+    const isNumberKeyPressedNow =
+      !isNaN(Number(event.key)) || event.code.startsWith('Digit');
+
+    if (!isNumberKeyPressedNow) {
+      return;
+    }
+
+    const { hotkey } = quickSlotsResponseRef.current;
+    const currentNumberKey = !isNaN(Number(event.key))
+      ? Number(event.key)
+      : Number(event.code.at(-1));
+
+    if (!isSlotNo(currentNumberKey)) {
+      return;
+    }
+
+    if (hotkey === 'Alt' && altPressed) {
+      console.log('perform random defense', currentNumberKey);
+      performRandomDefense(currentNumberKey, 'keydown');
+      return;
+    }
+
+    if (hotkey === 'F2' && f2Pressed) {
+      performRandomDefense(currentNumberKey, 'keydown');
+    }
+  };
+
+  const deletePressedKeyInfo = (event: globalThis.KeyboardEvent) => {
+    const key = event.key;
+    const code = event.code;
+
+    pressedKeysRef.current.delete(key);
+    pressedCodesRef.current.delete(code);
+
+    console.log(pressedKeysRef.current, pressedCodesRef.current);
+  };
+
+  const performRandomDefense = async (
+    selectedSlotNo: SlotNo,
+    method: 'keydown' | 'click',
+  ) => {
+    console.log('enter', isRandomDefenseAvailableRef.current);
+    if (!isRandomDefenseAvailableRef.current) {
+      return;
+    }
+
+    isRandomDefenseAvailableRef.current = false;
+    setIsRandomDefenseAvailable(false);
+
+    console.log('start', quickSlotsResponseRef.current);
+
+    const { slots } = quickSlotsResponseRef.current;
+
+    console.log('slots', slots);
+    const selectedSlot = slots[selectedSlotNo];
+
+    console.log('selectedSlot', selectedSlot);
+
+    if (selectedSlot.isEmpty) {
+      if (method === 'click') {
+        onToast(
+          {
+            title: `${selectedSlotNo}번 슬롯은 현재 비어 있습니다.`,
+            mainIconSrc: chrome.runtime.getURL('dice.png'),
+            descriptions: [
+              '추첨을 만들지 않으셨다면, 설정에서 해당 슬롯에 추첨을 먼저 만들어 주세요!',
+              '설정의 퀵슬롯 메뉴에서 선택된 슬롯 번호를 변경하는 것으로 위젯 클릭 시 사용할 추첨의 슬롯을 정하실 수 있습니다.',
+            ],
+          },
+          8000,
+        );
+      }
+      isRandomDefenseAvailableRef.current = true;
+      setIsRandomDefenseAvailable(true);
+
+      return;
+    }
+
+    const randomDefenseResultResponse = await chrome.runtime.sendMessage({
+      command: COMMANDS.GET_RANDOM_DEFENSE_RESULT,
+      query: selectedSlot.query,
+    });
+
+    if (!isRandomDefenseResultResponse(randomDefenseResultResponse)) {
+      onToast(
+        {
+          title: '데이터 불일치가 발견되었습니다.',
+          mainIconSrc: chrome.runtime.getURL('dice.png'),
+          descriptions: '개발자에게 이 문제가 발생했음을 알려 주세요.',
+        },
+        8000,
+      );
+      console.log(randomDefenseResultResponse);
+      isRandomDefenseAvailableRef.current = true;
+      setIsRandomDefenseAvailable(true);
+      return;
+    }
+
+    if (!randomDefenseResultResponse.success) {
+      const { errorMessage, errorDescriptions } = randomDefenseResultResponse;
+
+      onToast(
+        {
+          title: errorMessage,
+          mainIconSrc: chrome.runtime.getURL('dice.png'),
+          descriptions: errorDescriptions,
+        },
+        8000,
+      );
+      isRandomDefenseAvailableRef.current = true;
+      setIsRandomDefenseAvailable(true);
+
+      return;
+    }
+
+    const { problemInfo } = randomDefenseResultResponse;
+    const { problemId } = problemInfo;
+
+    location.href = `https://acmicpc.net/problem/${problemId}`;
+  };
+
+  const performRandomDefenseByClick = () => {
+    performRandomDefense(quickSlotsResponseRef.current.selectedSlotNo, 'click');
+  };
+
   return {
     isRandomDefenseAvailable,
-    performRandomDefense,
+    performRandomDefenseByClick,
   };
 };
 

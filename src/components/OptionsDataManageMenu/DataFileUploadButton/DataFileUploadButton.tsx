e33639c9a126e29e77b418b7405e11b4aaaec021
WizardRabbit <curious.wzrabbit@gmail.com>
2024-09-08 12:53:01 +0900

    ✨ feat: DataFileUploadButton 컴포넌트를 구현

---
+++ src/components/OptionsDataManageMenu/DataFileUploadButton/DataFileUploadButton.tsx
@@ -0,0 +1,32 @@
+import type { MouseEvent, ChangeEvent } from 'react';
+import { FileUploadIcon } from '~images/svg';
+import * as S from './DataFileUploadButton.styled';
+
+interface DataFileUploadButtonProps {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const DataFileUploadButton = (props: DataFileUploadButtonProps) => {
+  const { onChange } = props;
+
+  return (
+    <S.Container>
+      <S.FakeUploadButton role="button">
+        <S.UploadIconWrapper>
+          <FileUploadIcon />
+        </S.UploadIconWrapper>
+        <S.Text>설정 데이터 업로드</S.Text>
+      </S.FakeUploadButton>
+      <S.UploadInput
+        type="file"
+        accept=".ttj"
+        onClick={(event: MouseEvent<HTMLInputElement>) => {
+          event.currentTarget.value = '';
+        }}
+        onChange={onChange}
+      />
+    </S.Container>
+  );
+};
+
+export default DataFileUploadButton;

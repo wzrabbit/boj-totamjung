9c4b232d2e209d9584e0eb8ae46df78c67f12d88
요술토끼 <curious.wzrabbit@gmail.com>
2024-10-08 16:44:15 +0900

    ✨ feat: 간편 모드로 추첨을 생성할 때 생성되는 쿼리를 간단화

--- domains/randomDefense/randomDefenseQueryGenerator.ts
+++ domains/randomDefense/randomDefenseQueryGenerator.ts
@@ -43,7 +43,7 @@ export const generateRandomDefenseQuery = (
       )})`
     : '';
 
-  return `(*0&s?false|!*0) o?true w?false ${handleBanQuery}${solvedRangeQuery}${difficultyRangeQuery}${algorithmNamesQuery}`.trim();
+  return `(*0&!s?|!*0) o? -w? ${handleBanQuery}${solvedRangeQuery}${difficultyRangeQuery}${algorithmNamesQuery}`.trim();
 };
 
 const generateAlgorithmTags = (algorithmIds: number[]) => {

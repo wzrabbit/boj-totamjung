9213c1cde9d3eef47dae170ddd4a91eb23b47b10
요술토끼 <curious.wzrabbit@gmail.com>
2024-10-14 10:11:39 +0900

    ✨ feat: 토탐정 설치 시 가이드 페이지를 표시
    Co-authored-by: junah201 <junah.dev@gmail.com>

--- entrypoints/background/main.ts
+++ entrypoints/background/main.ts
@@ -43,10 +43,14 @@ import { isUserSolvedProblem } from '@/domains/tierHider/userSolvedChecker';
 import { getRandomDefenseResult } from '@/domains/randomDefense/randomDefenseProblemChooser';
 import { initializeDataOnFirstInstall } from '@/domains/dataHandlers/dataInitializer';
 
+const totamjungGuideUrl =
+  'https://github.com/wzrabbit/boj-totamjung/wiki/%ED%86%A0%ED%83%90%EC%A0%95-%EC%82%AC%EC%9A%A9-%EA%B0%80%EC%9D%B4%EB%93%9C';
+
 const executeBackground = () => {
   browser.runtime.onInstalled.addListener(({ reason }) => {
     if (reason === 'install') {
       initializeDataOnFirstInstall();
+      browser.tabs.create({ url: totamjungGuideUrl });
       return;
     }
 

7129dab45479f59073452ee8dd5c96146f4a62bb
wzrabbit <curious.wzrabbit@gmail.com>
2024-12-17 19:54:49 +0900

    🧪 test: ProblemCardGrid 컴포넌트에 대응하는 스토리북을 작성

---
+++ components/ProblemCardGrid/ProblemCardGrid.stories.tsx
@@ -0,0 +1,134 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ProblemCardGrid from './ProblemCardGrid';
+import type { Tier } from '@/types/randomDefense';
+
+/**
+ * `ProblemCardGrid`
+ */
+const meta = {
+  title: 'components/QuickSlotMenu/ProblemCardGrid',
+  component: ProblemCardGrid,
+} satisfies Meta<typeof ProblemCardGrid>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+interface ProblemInfo {
+  problemId: number;
+  title: string;
+  tier: Tier;
+}
+
+const getSampleProblemInfos = (problemCount: number): ProblemInfo[] => {
+  return Array.from({ length: problemCount }).map((_, index) => ({
+    problemId: 40000 + index,
+    title: `${40000 + index}번 문제`,
+    tier: 0,
+  }));
+};
+
+export const CardCount6: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(6),
+  },
+};
+
+export const CardCount1: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(1),
+  },
+};
+
+export const CardCount2: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(2),
+  },
+};
+
+export const CardCount5: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(5),
+  },
+};
+
+export const CardCount10: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(10),
+  },
+};
+
+export const CardCount25: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(25),
+  },
+};
+
+export const CardCount50: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '80vh' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  args: {
+    problemInfos: getSampleProblemInfos(50),
+  },
+};

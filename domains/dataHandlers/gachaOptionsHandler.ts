231bad783c56e96b2e95ee2fb88e7435d588fbc2
wzrabbit <curious.wzrabbit@gmail.com>
2025-01-03 00:49:58 +0900

    ✨ feat: 즉석 추첨에 대응하는 Validator, Sanitizer, dataHandler를 구현

---
+++ domains/dataHandlers/gachaOptionsHandler.ts
@@ -0,0 +1,20 @@
+import { STORAGE_KEY } from '@/constants/commands';
+import { isGachaOptionsResponse } from './validators/gachaOptionsValidator';
+import { sanitizeGachaOptionsResponse } from './sanitizers/gachaOptionsSanitizer';
+import type { GachaOptionsResponse } from '@/types/gacha';
+
+export const fetchGachaOptions = async (): Promise<GachaOptionsResponse> => {
+  const data = await browser.storage.local.get(STORAGE_KEY.GACHA_OPTIONS);
+
+  return sanitizeGachaOptionsResponse(data);
+};
+
+export const saveGachaOptions = (gachaOptions: unknown) => {
+  if (!isGachaOptionsResponse(gachaOptions)) {
+    return;
+  }
+
+  browser.storage.local.set({
+    [STORAGE_KEY.FONT_NO]: gachaOptions,
+  });
+};

d37b0e4ca24e956a84b00006a1b9e6962a695421
wzrabbit <wzrabbit@users.noreply.github.com>
2025-04-22 16:08:38 +0000

    deploy: 3b2234d76c5d2ea2d295590982ba8dcbacd471cb

---
+++ assets/HiderSection-CW4PsfRB.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{M as s}from"./MenuTitle-CeT6tPAk.js";import{S as n,A as l}from"./AlgorithmPool-CMtNJS9L.js";import{H as a}from"./HiderFieldsetMenu-o5Vcvl5T.js";import"./index-RYns6xqu.js";import{d as i}from"./styled-components.browser.esm-CkAH7aAm.js";const c=i.section`
+  display: flex;
+  column-gap: 20px;
+  flex-shrink: 0;
+`,d=i.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 6px;
+`,r=o=>{const{show:t}=o;return t?e.jsxs(c,{children:[e.jsxs(d,{children:[e.jsx(s,{title:"알고 있는 알고리즘 관리하기",iconSrc:e.jsx(n,{})}),e.jsx(l,{})]}),e.jsx(a,{})]}):null};try{r.displayName="HiderSection",r.__docgenInfo={description:"",displayName:"HiderSection",props:{show:{defaultValue:null,description:"",name:"show",required:!0,type:{name:"boolean"}}}}}catch{}export{r as H};

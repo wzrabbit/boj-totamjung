46e1079142c0551cc38b9d2851e0d172ae1b751d
wzrabbit <curious.wzrabbit@gmail.com>
2025-09-19 01:32:00 +0900

    ✨ feat: `<ProblemTimer>`에 테마를 적용할 수 있도록 기능 추가

--- components/ProblemTimer/ProblemTimer.styled.ts
+++ components/ProblemTimer/ProblemTimer.styled.ts
@@ -1,39 +1,105 @@
 import { styled } from 'styled-components';
+import { theme } from '@/styles/theme';
+import { getTransparentHexColor } from '@/utils/getTransparentHexColor';
+import type { MainTheme } from '@/types/mainTheme';
 
-export const Container = styled.div`
+const timerBackgroundColors: Record<MainTheme, string> = {
+  none: theme.color.BOJ_BLUE,
+  totamjung: theme.color.BROWN,
+  bojExtendedDark: theme.bojExtendedColor.DARKEST_GRAY,
+  bojExtendedRigel: theme.bojExtendedColor.SKY_BLUE,
+  solvedAcLight: theme.solvedAcColor.OFF_WHITE,
+  solvedAcDark: theme.solvedAcColor.INDIGO,
+  solvedAcBlack: theme.solvedAcColor.DARK_INDIGO,
+};
+
+const timerBorderColors: Record<MainTheme, string> = {
+  none: theme.color.BOJ_BLUE,
+  totamjung: theme.color.LIGHTER_BROWN,
+  bojExtendedDark: theme.bojExtendedColor.DARK_GRAY,
+  bojExtendedRigel: theme.bojExtendedColor.SKY_BLUE,
+  solvedAcLight: theme.solvedAcColor.LIGHT_GRAY,
+  solvedAcDark: theme.solvedAcColor.GRAY,
+  solvedAcBlack: theme.solvedAcColor.GRAY,
+};
+
+const timerTextColors: Record<MainTheme, string> = {
+  none: theme.color.WHITE,
+  totamjung: theme.color.BEIGE,
+  bojExtendedDark: theme.bojExtendedColor.LIGHT_GRAY,
+  bojExtendedRigel: theme.color.WHITE,
+  solvedAcLight: theme.color.BLACK,
+  solvedAcDark: theme.solvedAcColor.OFF_WHITE,
+  solvedAcBlack: theme.solvedAcColor.OFF_WHITE,
+};
+
+const timerIconColors: Record<MainTheme, string> = {
+  none: theme.color.WHITE,
+  totamjung: theme.color.LIGHTEST_BROWN,
+  bojExtendedDark: theme.bojExtendedColor.LIGHT_GRAY,
+  bojExtendedRigel: theme.color.WHITE,
+  solvedAcLight: theme.solvedAcColor.GRAY,
+  solvedAcDark: theme.solvedAcColor.LIGHT_GRAY,
+  solvedAcBlack: theme.solvedAcColor.LIGHT_GRAY,
+};
+
+export const circleProgressBarColors: Record<MainTheme, string> = {
+  none: theme.color.WHITE,
+  totamjung: theme.color.LIGHTEST_BROWN,
+  bojExtendedDark: theme.bojExtendedColor.LIGHT_GRAY,
+  bojExtendedRigel: theme.color.WHITE,
+  solvedAcLight: theme.solvedAcColor.GRAY,
+  solvedAcDark: theme.solvedAcColor.LIGHT_GRAY,
+  solvedAcBlack: theme.solvedAcColor.LIGHT_GRAY,
+};
+
+export const circleProgressBarTrackColors: Record<MainTheme, string> = {
+  none: getTransparentHexColor(theme.color.WHITE, 0.3),
+  totamjung: theme.color.DARK_BROWN,
+  bojExtendedDark: theme.color.BLACK,
+  bojExtendedRigel: getTransparentHexColor(theme.color.WHITE, 0.3),
+  solvedAcLight: getTransparentHexColor(theme.solvedAcColor.GRAY, 0.3),
+  solvedAcDark: theme.solvedAcColor.DARK_INDIGO,
+  solvedAcBlack: theme.color.BLACK,
+};
+
+export const Container = styled.div<{ $timerTheme: MainTheme }>`
   display: inline-flex;
   align-items: center;
-  gap: 8px;
   padding: 4px;
   height: 40px;
 
-  border: 2px solid ${({ theme }) => theme.color.LIGHTEST_BROWN};
-  background-color: ${({ theme }) => theme.color.BROWN};
+  border: 2px solid ${({ $timerTheme }) => timerBorderColors[$timerTheme]};
+  background-color: ${({ $timerTheme }) => timerBackgroundColors[$timerTheme]};
   border-radius: 20px;
 
   user-select: none;
 `;
 
-export const TimeDisplay = styled.span`
-  color: ${({ theme }) => theme.color.LIGHTEST_BROWN};
-  font-size: 24px;
-  font-weight: 700;
+export const TimeDisplay = styled.span<{ $timerTheme: MainTheme }>`
+  margin: 0 2px 0 6px;
+
+  color: ${({ $timerTheme }) => timerTextColors[$timerTheme]};
+  font-size: 20px;
+  font-weight: 600;
   font-variant-numeric: tabular-nums;
 `;
 
 export const ButtonContainer = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: center;
-  gap: 2px;
+
+  width: 50px;
 `;
 
-export const ControlButton = styled.button`
+export const ControlButton = styled.button<{ $timerTheme: MainTheme }>`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  width: 24px;
-  height: 24px;
+  width: 26px;
+  height: 26px;
 
   background: transparent;
 
@@ -48,6 +114,6 @@ export const ControlButton = styled.button`
     width: 100%;
     height: 100%;
 
-    color: ${({ theme }) => theme.color.LIGHTEST_BROWN};
+    color: ${({ $timerTheme }) => timerIconColors[$timerTheme]};
   }
 `;

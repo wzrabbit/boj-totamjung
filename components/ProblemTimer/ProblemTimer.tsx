6badcec1af385c592820310f0d6f6afae810b79d
wzrabbit <curious.wzrabbit@gmail.com>
2025-09-23 17:14:50 +0900

    ♻️ refactor: `<ProblemTimer>`를 `<ProblemTimerControls>`을 사용한 코드로 리팩터링

--- components/ProblemTimer/ProblemTimer.tsx
+++ components/ProblemTimer/ProblemTimer.tsx
@@ -1,8 +1,6 @@
 import * as S from './ProblemTimer.styled';
+import ProblemTimerControls from '@/components/ProblemTimerControls';
 import CircleProgressBar from '@/components/common/CircleProgressBar';
-import { PlayIcon, PauseIcon, StopIcon, ClockEditIcon } from '@/assets/svg';
-import { theme } from '@/styles/theme';
-import { getTransparentHexColor } from '@/utils/getTransparentHexColor';
 import type { MainTheme } from '@/types/mainTheme';
 
 interface ProblemTimerProps {
@@ -15,26 +13,11 @@ interface ProblemTimerProps {
   onPlay: () => void;
   onPause: () => void;
   onStop: () => void;
-  onTimeEdit: () => void;
+  onEdit: () => void;
 }
 
 const ProblemTimer = (props: ProblemTimerProps) => {
-  const {
-    hours,
-    minutes,
-    seconds,
-    progress,
-    status,
-    theme: mainTheme,
-    onPlay,
-    onPause,
-    onStop,
-    onTimeEdit,
-  } = props;
-
-  const timeString = [hours, minutes, seconds]
-    .map((value) => String(value).padStart(2, '0'))
-    .join(':');
+  const { progress, theme: mainTheme, ...rest } = props;
 
   return (
     <S.Container $timerTheme={mainTheme}>
@@ -45,45 +28,12 @@ const ProblemTimer = (props: ProblemTimerProps) => {
         color={S.circleProgressBarColors[mainTheme]}
         trackColor={S.circleProgressBarTrackColors[mainTheme]}
       />
-      <S.TimeDisplay $timerTheme={mainTheme}>{timeString}</S.TimeDisplay>
-      <S.ButtonContainer>
-        {status === 'play' && (
-          <S.ControlButton
-            $timerTheme={mainTheme}
-            aria-label="문제 타이머 일시정지하기"
-            onClick={onPause}
-          >
-            <PauseIcon />
-          </S.ControlButton>
-        )}
-        {status === 'pause' && (
-          <S.ControlButton
-            $timerTheme={mainTheme}
-            aria-label="문제 타이머 시작하기"
-            onClick={onPlay}
-          >
-            <PlayIcon />
-          </S.ControlButton>
-        )}
-        {status !== 'stop' && (
-          <S.ControlButton
-            $timerTheme={mainTheme}
-            aria-label="문제 타이머 정지하기"
-            onClick={onStop}
-          >
-            <StopIcon />
-          </S.ControlButton>
-        )}
-        {status === 'stop' && (
-          <S.ControlButton
-            $timerTheme={mainTheme}
-            aria-label="문제 타이머 시간 설정하기"
-            onClick={onTimeEdit}
-          >
-            <ClockEditIcon />
-          </S.ControlButton>
-        )}
-      </S.ButtonContainer>
+      <ProblemTimerControls
+        {...rest}
+        color={S.timerTextColors[mainTheme]}
+        height={40}
+        hasDeleteButton={false}
+      />
     </S.Container>
   );
 };

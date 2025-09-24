1a252b9f10fc1d5cabc9577ffe5c83db7f2e364e
wzrabbit <wzrabbit@users.noreply.github.com>
2025-09-24 10:37:19 +0000

    deploy: f903daca6660a8cfe11f84ddc70a6c32cd2b74e2

--- assets/CircleProgressBar.stories-DRV90_CX.js
+++
@@ -1,8 +0,0 @@
-import{C as t}from"./CircleProgressBar-DZqE1-bv.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";const m={title:"components/CircleProgressBar",component:t,argTypes:{size:{description:"프로그레스 바의 크기입니다."},progress:{description:"프로그레스 바가 얼마나 채워져 있는가를 의미합니다. **이 값은 0 이상 100 이하의 수여야 합니다.**",control:{type:"range",min:0,max:100}},color:{description:"프로그레스 바의, **채워져 있는 부분**의 색상입니다."},trackColor:{description:"프로그레스 바의, **채워져 있지 않은 부분**의 색상입니다."}},parameters:{docs:{description:{component:"`CircleProgressBar`는 진행 상황을 시각적으로 보여주는 컴포넌트입니다."}}}},r={args:{size:45,progress:65,color:"#ffe091",trackColor:"#362b28"}};var o,e,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 45,
-    progress: 65,
-    color: '#ffe091',
-    trackColor: '#362b28'
-  }
-}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,m as default};

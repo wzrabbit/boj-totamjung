83151b7c643b1ce8f2ea043ae1d7c957ecf1977d
wzrabbit <curious.wzrabbit@gmail.com>
2025-10-11 02:54:09 +0900

    ✨ feat: 검색 쿼리 설명을 수정
    - "서포터" 는 쓰이지 않는 용어이므로 "프로" 를 사용

--- components/QuickSlotsMenu/SlotEditModal/SlotEditModal.tsx
+++ components/QuickSlotsMenu/SlotEditModal/SlotEditModal.tsx
@@ -83,15 +83,14 @@ const SlotEditModal = (props: SlotEditModalProps) => {
         </S.Label>
         <S.InformationTextContainer>
           <Text type="normal" fontSize={14}>
-            solved.ac 검색 쿼리 작성법을 모르신다면,{' '}
             <TextLink href="https://solved.ac/search" fontSize={14}>
               solved.ac 문제 고급 검색
             </TextLink>{' '}
-            페이지를 확인해 보세요!
+            페이지를 통해 solved.ac 검색 쿼리 작성법을 확인하실 수 있습니다!
           </Text>
           <Text type="normal" fontSize={14}>
-            추첨은 비로그인 상태에서 진행되므로, 서포터 전용 쿼리는 사용할 수
-            없음에 유의해 주세요.
+            추첨은 비로그인 상태에서 진행되므로, 솔브드 프로 플랜 전용 쿼리는
+            사용하실 수 없습니다.
           </Text>
         </S.InformationTextContainer>
         <ErrorText fontSize={14} errorMessage={errorMessage} />

b7454b9353d1b2d72d01af7b972f2b8ca5c92842
wzrabbit <curious.wzrabbit@gmail.com>
2025-10-10 23:18:00 +0900

    ✨ feat: 미완성 쿼리에 한해 자동완성 적용 이후 공백이 생기지 않도록 예외처리

--- hooks/randomDefense/useQueryInput.ts
+++ hooks/randomDefense/useQueryInput.ts
@@ -3,26 +3,82 @@ import type { QuerySuggestion } from '@/types/randomDefense';
 import { useState, useEffect, useRef } from 'react';
 import type { ChangeEventHandler, KeyboardEventHandler } from 'react';
 
-const getLastTokenInfo = (text: string) => {
+/**
+ * solved.ac에서의 미완성 쿼리 목록입니다. 이 쿼리들은 단독으로 작동하지 않으며, 반드시 뒤에 내용이 와야 하므로
+ * 사용자가 이 쿼리들을 자동완성으로 고르는 경우 공백을 뒤에 두지 않아야 합니다.
+ */
+const partialCaptions = [
+  '*',
+  'tier:',
+  'id:',
+  's#',
+  'solved:',
+  '#',
+  'tag',
+  '/',
+  'from:',
+  't#',
+  'average_try:',
+  'µ#',
+  '%',
+  'lang:',
+  '@',
+  'solved_by:',
+  'o@',
+  'solved_by_org',
+  't@',
+  'tried_by:',
+  'v@',
+  'voted_by:',
+  'c@',
+  'contributed_by:',
+  'c/',
+  'in_class:',
+  'e/',
+  'in_class_essentials:',
+  's?',
+  'standard:',
+  'p?',
+  'sprout:',
+  'sp?',
+  'o?',
+  'solvable:',
+  'v?',
+  'votable:',
+  'c?',
+  'contributable:',
+  'w?',
+  'warning:',
+  'v#',
+  'voted:',
+  'c#',
+  'contributed:',
+  '**',
+  'vote_average:',
+  'a*',
+  'µ*',
+];
+
+const getLastTokenStartIndex = (text: string) => {
   const plainTextRegex = /^[A-z가-힣 ]+$/;
   const delimiters = ['|', '&', '(', ')', '!', '~', '-', ' '];
 
   if (plainTextRegex.test(text)) {
-    return { token: text, startIndex: 0 };
+    return 0;
   }
 
   for (let index = text.length - 1; index >= 0; index -= 1) {
     if (delimiters.includes(text[index])) {
-      return { token: text.slice(index + 1), startIndex: index + 1 };
+      return index + 1;
     }
   }
-  return { token: text, startIndex: 0 };
+  return 0;
 };
 
 const autocomplete = (text: string, caption: string) => {
-  const { startIndex } = getLastTokenInfo(text);
+  const startIndex = getLastTokenStartIndex(text);
 
-  return `${text.slice(0, startIndex)}${caption} `;
+  return `${text.slice(0, startIndex)}${caption}${partialCaptions.includes(caption) ? '' : ' '}`;
 };
 
 interface useQueryInputParams {

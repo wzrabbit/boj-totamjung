5a993bf12570b30d6d6671c3864b4f0071f3deac
wzrabbit <wzrabbit@users.noreply.github.com>
2025-10-11 02:33:24 +0000

    deploy: e12af2093c4fe20c2ba0b9aada5d23a9d1f17240

---
+++ assets/RandomDefenseCreateMenu.stories-CFWUnGDN.js
@@ -0,0 +1,11 @@
+import{R as m}from"./RandomDefenseCreateMenu-TUTKv9FI.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./NamedFrame-D2PEwBHE.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";import"./Input-Dv8CBI6y.js";import"./toPx-DeOSKary.js";import"./getTransparentHexColor-DN69VCBw.js";import"./theme-CqSsbPCW.js";import"./Textarea-jv6VaTTH.js";import"./TextLink-B35u5JYr.js";import"./ErrorText-DEoOg6NU.js";import"./warning-BLusDAjJ.js";import"./Text-D2wNJgtT.js";import"./RandomDefenseCreateButton-DGeX3Iqd.js";import"./RandomDefenseCapsuleButton-DhA1nxYd.js";import"./DifficultyAdjustMenu-l4nDvGZj.js";import"./TierSlider-B-Wa-Cg5.js";import"./typeGuards-Bsa9ERJp.js";import"./TierRange-BhJJoGeE.js";import"./index-CElCL35T.js";import"./TierPresetButtonList-hL54dKj2.js";import"./TierPresetButton-2o2nynyl.js";import"./SearchOperatorSelect-Dbw-UY9n.js";import"./check-BIa-lIjI.js";import"./AlgorithmSearchInput-DndG9i4L.js";import"./MiniAlgorithmButton-Cu511WHt.js";import"./getSearchResults-B-QY19XD.js";import"./algorithmInfos-w49mooiO.js";import"./randomDefense-BrQ0TErT.js";import"./Select-CMYR-bFx.js";import"./useSelect-Cvrmd0wu.js";const z={title:"components/RandomDefenseCreateMenu",component:m,argTypes:{selectedSlotNo:{description:"현재 선택되어 있는 슬롯의 번호입니다."},isLoaded:{description:"본 컴포넌트가 로드되었는지를 의미합니다."},onSubmit:{description:"사용자가 추첨 생성 버튼을 눌러 새로운 추첨 결과를 생성하기 위해 정보를 보낼 때 실행시킬 콜백 함수입니다."}},parameters:{docs:{description:{component:"`RandomDefenseCreateMenu`는 추첨 생성을 위한 메뉴입니다."}}}},o={args:{selectedSlotNo:1,isLoaded:!0,onSubmit:i=>{alert(`이 알림창이 떴다는 것은 검증 결과 [성공]을 의미합니다. 이제 부모로부터 전달받은 아래의 함수를 실행해, 본격적인 추첨 생성 작업을 시작하겠죠.
+
+onSubmit(${JSON.stringify(i)})`)}}};var t,r,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    selectedSlotNo: 1,
+    isLoaded: true,
+    onSubmit: randomDefenseFormData => {
+      alert(\`이 알림창이 떴다는 것은 검증 결과 [성공]을 의미합니다. 이제 부모로부터 전달받은 아래의 함수를 실행해, 본격적인 추첨 생성 작업을 시작하겠죠.\\n\\nonSubmit(\${JSON.stringify(randomDefenseFormData)})\`);
+    }
+  }
+}`,...(e=(r=o.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const A=["Default"];export{o as Default,A as __namedExportsOrder,z as default};
--- assets/SearchOperatorSelect.stories-QYnIP1nb.js
+++
@@ -1,6 +0,0 @@
-import{S as a}from"./SearchOperatorSelect-C0lH7hGi.js";import{f as s}from"./index-BAc7KiQ7.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./index--qcDGAq6.js";import"./check-BIa-lIjI.js";import"./styled-components.browser.esm-CIS6JKSM.js";const f={title:"components/RandomDefenseCreateMenu/SearchOperatorSelect",component:a,argTypes:{searchOperator:{description:"선택되어 있는 **연산자**를 의미합니다. **연산자**의 값이 어떤가에 따라 추첨 생성 시 생성되는 쿼리의 방식이 달라집니다. `AND`가 선택된 경우에는 해당되는 알고리즘을 포함하는 경우, `OR`는 해당되는 알고리즘 중 적어도 하나를 포함하는 경우, `NOR`는 해당되는 알고리즘을 모두 포함하지 않는 경우를 의미합니다."},onClick:{description:"연산자가 변경되어야 할 경우 실행시킬 콜백 함수입니다."}},parameters:{docs:{description:{component:"`SearchOperatorSelect`는 추첨 생성 폼에서, 검색에 포함할 알고리즘들의 검색 방법을 정할 수 있는 컴포넌트입니다. 보다 구체적으로는, 여러 개의 알고리즘을 쿼리에 포함시킬 때, 사용할 연산자(`|`, `&`, `-`)를 정할 수 있습니다."}}}},e={args:{searchOperator:"AND",onClick:s()}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    searchOperator: 'AND',
-    onClick: fn()
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const O=["Default"];export{e as Default,O as __namedExportsOrder,f as default};

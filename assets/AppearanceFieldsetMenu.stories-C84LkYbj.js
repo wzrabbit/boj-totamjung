ac60d63108eb13f06b2ccb8ffbf39709e608bb76
wzrabbit <wzrabbit@users.noreply.github.com>
2025-10-14 10:29:20 +0000

    deploy: 9c3d87c7a181eb4291697b9c52f7e351860abf3c

--- assets/AppearanceFieldsetMenu.stories-C84LkYbj.js
+++
@@ -1,7 +0,0 @@
-import{A as p}from"./AppearanceFieldsetMenu-Bt3hKkzC.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./MenuTitle-C7FRvFNi.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";import"./Fieldset-8hVaSIv3.js";import"./Radio-BjiebFTg.js";import"./Text-Bm9VDKPH.js";import"./toPx-DeOSKary.js";import"./fontNoValidator-DiGyRE7l.js";import"./commands-BoKETmK5.js";const F={title:"components/AppearanceFieldsetMenu",component:p,parameters:{docs:{description:{component:`\`AppearanceFieldsetMenu\`는 토탐정의 "외형" 관련 기능들에 대한 설정을 조작할 수 있는 필드셋 형식의 메뉴 컴포넌트입니다.
-
-해당 메뉴에서는 아래의 설정들을 조작할 수 있습니다.
-1. 테마 설정
-2. 문제 본문 폰트 설정`}}}},e={args:{}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {}
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const M=["Default"];export{e as Default,M as __namedExportsOrder,F as default};

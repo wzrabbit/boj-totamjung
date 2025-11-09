f0ca4272704b6e5b13d739b118ab99b8788b0e71
wzrabbit <wzrabbit@users.noreply.github.com>
2025-11-09 10:29:33 +0000

    deploy: 93c5f0a9f52671e4f837788fac4fccc1721edccb

---
+++ storybook/assets/Options.stories-DyU2902j.js
@@ -0,0 +1,42 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{O as a}from"./OptionsHeader-CP7tEAne.js";import{H as n}from"./HiderSection-NlEvvgH1.js";import{R as c}from"./RandomDefenseSection-NS4kkoSZ.js";import{A as d}from"./AppearanceAndDataManageSection-DGPjbJFA.js";import{r as l}from"./index--qcDGAq6.js";import{d as r,m as x}from"./styled-components.browser.esm-CIS6JKSM.js";import"./index-yBjzXJbu.js";import"./OptionsNav-oYhGpsQY.js";import"./SimpleModal-DwdThE8F.js";import"./Modal-JaXjAa2V.js";import"./theme-CqSsbPCW.js";import"./close-BziWfrbF.js";import"./index-B5qaPpLJ.js";import"./index-B6ujFmsw.js";import"./IconButton-CJ4kdUS9.js";import"./close-circle-Dx72CTk1.js";import"./check-circle-B-abXz3w.js";import"./TotamjungInfoModal-D15IATKV.js";import"./Text-D2wNJgtT.js";import"./toPx-DeOSKary.js";import"./useModal-BJrESIr1.js";import"./MenuTitle-C7FRvFNi.js";import"./AlgorithmPool-Cq3OS4OD.js";import"./AlgorithmList-BJmla8pb.js";import"./AlgorithmListItem-B_Sapc5k.js";import"./Checkbox-NPditMri.js";import"./FallbackWithImage-CiH6iRLC.js";import"./Loading-DfWylf4Y.js";import"./loading-Ckb1CsUZ.js";import"./getSearchResults-B-QY19XD.js";import"./algorithmInfos-w49mooiO.js";import"./commands-BoKETmK5.js";import"./checkedAlgorithmIdsValidator-D3rPSOXE.js";import"./checkedAlgorithmIdsSanitizer-DLz5h8R0.js";import"./defaultValues-BGh_tAG0.js";import"./no-search-result-CRbCEMin.js";import"./HiderFieldsetMenu-DFzLVAPD.js";import"./Fieldset-BpJQsBxJ.js";import"./Radio-BjiebFTg.js";import"./TierSelect-BRJqgYqJ.js";import"./check-BIa-lIjI.js";import"./useSelect-Cvrmd0wu.js";import"./index-CElCL35T.js";import"./ProblemTagLockTimer-r96yvMh3.js";import"./Input-BHr0Vq7L.js";import"./getTransparentHexColor-DN69VCBw.js";import"./hidden-tier-badge-CukGKv-l.js";import"./hiderOptionsValidator-CGMdHXNl.js";import"./typeGuards-Bsa9ERJp.js";import"./RandomDefenseCreateMenu-CUlFifq5.js";import"./NamedFrame-D2PEwBHE.js";import"./TextLink-B35u5JYr.js";import"./ErrorText-DEoOg6NU.js";import"./warning-BLusDAjJ.js";import"./RandomDefenseCreateButton-DGeX3Iqd.js";import"./RandomDefenseCapsuleButton-DhA1nxYd.js";import"./DifficultyAdjustMenu-1KFXuPNx.js";import"./TierSlider-B-Wa-Cg5.js";import"./TierRange-BhJJoGeE.js";import"./TierPresetButtonList-BKVxSFxO.js";import"./TierPresetButton-TxMIxK-u.js";import"./SearchOperatorSelect-BKZoQCEw.js";import"./AlgorithmSearchInput-D7Le8_1n.js";import"./MiniAlgorithmButton-Cu511WHt.js";import"./randomDefense-DE7lwrZG.js";import"./Select-CMYR-bFx.js";import"./QueryInput-CHvZ9okc.js";import"./QuickSlotsMenu-DbukDWgX.js";import"./SlotPagination-BV704-gL.js";import"./SlotInfo-Bm7kob_Q.js";import"./HotkeySwitcher-BKjIVDrx.js";import"./SlotEditModal-DSMtcvkN.js";import"./trash-JRBuQ8nU.js";import"./copy-BcJBRJXh.js";import"./RandomDefenseGachaModal-B1jE6HJ8.js";import"./CardBox-DLtDsh26.js";import"./index-BpuMuliS.js";import"./ProblemCardGrid-CfwfIOoR.js";import"./ProblemCard-0TlVoL6R.js";import"./gachaOptionsValidator-CX_fe7zV.js";import"./GachaModalNotification-DM5b6Hdn.js";import"./GachaProblemCountInputModal-CkdX5M9F.js";import"./useRandomDefenseSection-BCWo8uMU.js";import"./quickSlotsSanitizer-oZKigjIx.js";import"./quickSlotsValidator-CSlQ7P7m.js";import"./RandomDefenseHistoryMenu-OwXQFhH7.js";import"./RandomDefenseHistoryList-B78fE5FN.js";import"./RandomDefenseHistoryItem-B4HMGLVW.js";import"./clock-B5z8M2k5.js";import"./formatDate-DtmnLriP.js";import"./isTierHiddenSanitizer-BOyoG-TY.js";import"./Switch-Bs6u4NSz.js";import"./package-pspu3cL5.js";import"./AppearanceFieldsetMenu-77TdL_O1.js";import"./fontNoValidator-DiGyRE7l.js";import"./OptionsDataManageMenu-CDpkX45X.js";import"./OptionsDataResetModal-B7WyLc85.js";import"./useConfirmInput-CTEujkIa.js";import"./OptionsDataUploadModal-Cn4ewACr.js";import"./file-upload-mz0LOrA9.js";import"./isShouldShowWelcomeMessageDataValidator-BKshx6sc.js";import"./DataFileUploadButton-D46EKoSS.js";const g=""+new URL("totamjung-BrXdXI1T.png",import.meta.url).href,f=()=>{const[o,p]=l.useState("algorithmHider");return{selectedCategory:o,setSelectedCategory:p}},h=x`
+  0% {
+    transform: scaleY(100%);
+  }
+
+  50% {
+    transform: scaleY(97%);
+  }
+
+  100% {
+    transform: scaleY(100%);
+  }
+`,u=r.div`
+  display: flex;
+
+  width: 1310px;
+  height: 665px;
+  margin: 10px 0 0 20px;
+`,j=r.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+`,w=r.main`
+  display: flex;
+  column-gap: 20px;
+
+  width: 1060px;
+  height: 580px;
+`,y=r.div`
+  display: flex;
+  align-items: flex-end;
+
+  width: 250px;
+`,O=r.img`
+  width: 250px;
+  height: 331px;
+
+  transform-origin: bottom;
+  animation: ${h} 3s forwards infinite;
+`,C=()=>{const{selectedCategory:o,setSelectedCategory:p}=f();return t.jsxs(u,{children:[t.jsxs(j,{children:[t.jsx(a,{selectedCategory:o,onCategoryChange:p}),t.jsxs(w,{children:[t.jsx(n,{show:o==="algorithmHider"}),t.jsx(c,{show:o==="randomDefense"}),t.jsx(d,{show:o==="appearanceAndDataManage"})]})]}),t.jsx(y,{children:t.jsx(O,{src:g,alt:""})})]})},wo={title:"components/Options",component:C,parameters:{docs:{description:{component:"`Options`는 토탐정의 설정 페이지 전체를 구성하는 컴포넌트입니다."}}}},i={args:{}};var m,e,s;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {}
+}`,...(s=(e=i.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const yo=["Default"];export{i as Default,yo as __namedExportsOrder,wo as default};
---
+++ storybook/assets/RandomDefenseHistoryItem.stories-CzsDKB6h.js
@@ -0,0 +1,91 @@
+import{R as j}from"./RandomDefenseHistoryItem-B4HMGLVW.js";import{f as e}from"./index-BAc7KiQ7.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";import"./index-CElCL35T.js";import"./clock-B5z8M2k5.js";import"./trash-JRBuQ8nU.js";import"./formatDate-DtmnLriP.js";const Y={title:"components/RandomDefenseHistoryMenu/RandomDefenseHistoryItem",component:j,argTypes:{problemId:{description:"이 추첨 기록의 문제 번호입니다."},title:{description:"이 추첨 기록의 문제의 제목입니다."},tier:{description:"이 추첨 기록의 문제에 책정되어 있는 난이도(티어)입니다."},createdAt:{description:"해당 문제에 대응되는 추첨 결과가 실행된 시각입니다."},isHidden:{description:"이 추첨 기록의 문제 티어가 가려져 있는지의 여부입니다."},onDelete:{description:"이 추첨 기록을 지워야 할 경우 실행시킬 콜백 함수입니다."}},parameters:{docs:{description:{component:"`RandomDefenseHistoryItem`는 추첨 기록에서 추첨된 문제 하나의 정보를 보여주는 컴포넌트입니다."}}}},r={args:{problemId:27959,title:"초코바",tier:1,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},t={args:{problemId:27964,title:"콰트로치즈피자",tier:6,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},n={args:{problemId:27943,title:"가지 사진 찾기",tier:11,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},s={args:{problemId:27470,title:"멋진 부분집합",tier:16,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},a={args:{problemId:30243,title:"🧩 N-Queen (Hard)",tier:21,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},o={args:{problemId:31442,title:"좋은 수열",tier:26,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},i={args:{problemId:1223,title:"마법의 돌",tier:0,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},d={args:{problemId:27903,title:"인생",tier:31,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}},c={args:{problemId:29727,title:"선이 하나 더ㅠㅠ",tier:1,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!0,onDelete:e()}},l={args:{problemId:15898,title:"피아의 아틀리에 ~신비한 대회의 연금술사~",tier:15,createdAt:"2025-01-01T23:35:00.123Z",isHidden:!1,onDelete:e()}};var m,p,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    problemId: 27959,
+    title: '초코바',
+    tier: 1,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(u=(p=r.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var f,g,H;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    problemId: 27964,
+    title: '콰트로치즈피자',
+    tier: 6,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(H=(g=t.parameters)==null?void 0:g.docs)==null?void 0:H.source}}};var D,b,I;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    problemId: 27943,
+    title: '가지 사진 찾기',
+    tier: 11,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(I=(b=n.parameters)==null?void 0:b.docs)==null?void 0:I.source}}};var T,A,Z;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    problemId: 27470,
+    title: '멋진 부분집합',
+    tier: 16,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(Z=(A=s.parameters)==null?void 0:A.docs)==null?void 0:Z.source}}};var S,R,y;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    problemId: 30243,
+    title: '🧩 N-Queen (Hard)',
+    tier: 21,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(y=(R=a.parameters)==null?void 0:R.docs)==null?void 0:y.source}}};var N,v,x;o.parameters={...o.parameters,docs:{...(N=o.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    problemId: 31442,
+    title: '좋은 수열',
+    tier: 26,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(x=(v=o.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var z,B,G;i.parameters={...i.parameters,docs:{...(z=i.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    problemId: 1223,
+    title: '마법의 돌',
+    tier: 0,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(G=(B=i.parameters)==null?void 0:B.docs)==null?void 0:G.source}}};var L,P,Q;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    problemId: 27903,
+    title: '인생',
+    tier: 31,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(Q=(P=d.parameters)==null?void 0:P.docs)==null?void 0:Q.source}}};var U,_,E;c.parameters={...c.parameters,docs:{...(U=c.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    problemId: 29727,
+    title: '선이 하나 더ㅠㅠ',
+    tier: 1,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: true,
+    onDelete: fn()
+  }
+}`,...(E=(_=c.parameters)==null?void 0:_.docs)==null?void 0:E.source}}};var M,O,h;l.parameters={...l.parameters,docs:{...(M=l.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    problemId: 15898,
+    title: '피아의 아틀리에 ~신비한 대회의 연금술사~',
+    tier: 15,
+    createdAt: '2025-01-01T23:35:00.123Z',
+    isHidden: false,
+    onDelete: fn()
+  }
+}`,...(h=(O=l.parameters)==null?void 0:O.docs)==null?void 0:h.source}}};const $=["Bronze","Silver","Gold","Platinum","Diamond","Ruby","Unrated","NotRatable","Hidden","LongTitle"];export{r as Bronze,a as Diamond,n as Gold,c as Hidden,l as LongTitle,d as NotRatable,s as Platinum,o as Ruby,t as Silver,i as Unrated,$ as __namedExportsOrder,Y as default};

70fbc44d11cfb96095e79c2bcad7d6a39454fd32
요술토끼(wizardrabbit) <curious.wzrabbit@gmail.com>
2025-11-10 00:26:53 +0900

    [DOCS] 토탐정 가이드 프로젝트를 생성 (#188)
    * 📦 chore: Astro/starlight 프로젝트 생성 및 워크스페이스로 지정
    
    * ✨ feat: 프로젝트의 폰트 및 색상 테마를 설정
    
    * ✨ feat: 메인 페이지 커스터마이징
    
    * ✨ feat: `<CardGrid>` 컴포넌트를 이용해 토탐정이 제공하는 기능에 대한 내용을 담도록 커스터마이징
    
    * 📝 docs: 문서의 각 페이지를 생성하고, title / description을 추가
    
    * 📦 chore: favicon 아이콘이 토탐정 아이콘으로 적용되도록 설정 진행
    
    * 📦 chore: 사이트의 description을 추가
    
    * 📦 chore: 문서 수정 버튼과 마지막 수정 시각이 보이도록 변경
    
    - 랜딩 페이지는 제외
    
    * 📝 docs: 문의/기여 페이지에 이전/다음 네비게이션을 숨기도록 변경
    
    * 📝 docs: 랜딩 페이지의 description을 SEO에 걸맞는 설명으로 바꾸고, 나머지 페이지의 description은 제거
    
    * 📦 chore: `docs` 워크스페이스 프로젝트에서의 명령어를 쉽게 쓸 수 있도록 스크립트 추가
    
    * 📦 chore: 스토리북 빌드 명령어로 `build:storybook` 도 허용
    
    * 📝 docs: "퀵 스타트" 페이지를 작성
    
    * 📝 docs: "다운로드" 페이지를 작성
    
    * 📝 docs: "토탐정 위젯" 기능 페이지를 작성
    
    * 📝 docs: "알고리즘 가리개" 기능 페이지를 작성
    
    * 📝 docs: "랜덤 디펜스" 기능 페이지를 작성
    
    * 📝 docs: "즉석 추첨" 기능 페이지를 작성
    
    * 📝 docs: "티어 가리개" 기능 페이지를 작성
    
    * 📝 docs: 그 외 기능들을 다루는 페이지를 작성
    
    * 📝 docs: 문의 페이지를 작성
    
    * 📝 docs: 기여 방법 임시 페이지를 작성
    
    * 📝 docs: 랜딩 페이지의 솔브드 서비스 용어를 통일
    
    * 📦 chore: `package-lock.json`을 업데이트
    
    * 📦 chore: Storybook의 컴포넌트 문서와 Starlight의 문서가 공존하여 배포되도록 워크플로우 수정
    
    * 📦 chore: 프로젝트의 배포 루트가 `development`인지 아닌지에 따라 변경되도록 설정
    
    * 🛠 fix: 잘못된 절대경로 링크를 상대경로 링크로 변경
    
    * 🛠 fix: 잘못된 편집 링크를 수정
    
    * 📦 chore: Google Search Console용 인증 파일을 업로드
    
    * 🎨 style: 테마 변경 버튼의 길이를 변경
    
    * 📦 chore: 현재 브랜치를 워크플로우에서 제외
    
    - 테스트를 위한 임시조치였음
    
    * 📦 chore: 메인 프로젝트와 문서 프로젝트가 컴파일 시 검사하는 영역을 분리, 워크플로우에 반영
    
    * 📦 chore: 워크플로우의 Node.js 버전을 업데이트
    
    * 📦 chore: Astro 프로젝트 컴파일 명령어를 `astro check`로 변경
    
    * 📦 chore: `package-lock.json`을 업데이트

---
+++ docs/astro.config.mjs
@@ -0,0 +1,64 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://wzrabbit.github.io',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/boj-totamjung',
+  integrations: [
+    starlight({
+      title: '토탐정',
+      description:
+        '토탐정은 백준에서 사용할 수 있는 확장 프로그램입니다. 알고리즘 분류와 랜덤 디펜스 관련 기능들을 주로 제공하고, 그 외에 여러 유용한 기능들도 제공합니다.',
+      editLink: {
+        baseUrl: 'https://github.com/wzrabbit/boj-totamjung/edit/main/docs',
+      },
+      locales: {
+        root: {
+          label: '한국어',
+          lang: 'ko',
+        },
+      },
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/wzrabbit/boj-totamjung',
+        },
+      ],
+      sidebar: [
+        {
+          label: '다운로드',
+          items: [{ label: '다운로드', slug: 'downloads' }],
+        },
+        {
+          label: '퀵 스타트',
+          items: [{ label: '퀵 스타트', slug: 'quick-start' }],
+        },
+        {
+          label: '기능',
+          items: [
+            { label: '토탐정 위젯', slug: 'features/widget' },
+            { label: '알고리즘 가리개', slug: 'features/algorithm-hider' },
+            { label: '랜덤 디펜스', slug: 'features/random-defense' },
+            { label: '티어 가리개', slug: 'features/tier-hider' },
+            { label: '즉석 추첨', slug: 'features/gacha' },
+            { label: '그 외 기능들', slug: 'features/miscellaneous' },
+          ],
+        },
+        {
+          label: '토탐정에 기여하기',
+          items: [{ label: '기여 방법', slug: 'contribute' }],
+        },
+        {
+          label: '문의하기',
+          items: [{ label: '문의하기', slug: 'contact' }],
+        },
+      ],
+      customCss: ['./src/fonts/font-face.css', './src/styles/custom.css'],
+      favicon: '/favicon.png',
+      lastUpdated: true,
+    }),
+  ],
+});

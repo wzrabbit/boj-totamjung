b267f6d5eff8fc3e5460db00f2128d82609b459a
wzrabbit <wzrabbit@users.noreply.github.com>
2025-11-09 10:57:01 +0000

    deploy: bb0c9189922f61fc38ec732757b4c977a57f38a0

---
+++ storybook/assets/ProblemCard.stories-BA_PeedA.js
@@ -0,0 +1,111 @@
+import{P as z}from"./ProblemCard-0TlVoL6R.js";import{f as e}from"./index-BAc7KiQ7.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./index-CElCL35T.js";import"./index--qcDGAq6.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index-BpuMuliS.js";const Q={title:"components/ProblemCard",component:z,argTypes:{problemInfo:{description:"정보를 표시할 문제의 정보입니다."},isTierHidden:{description:"난이도를 숨길 지의 여부입니다."},width:{description:"카드의 가로 길이입니다. 단위는 px입니다."}},parameters:{docs:{description:{component:"`ProblemCard`는 즉석 추첨 결과에서 나온 결과에서 하나의 문제를 시각적으로 보여주는 카드 모양의 컴포넌트입니다."}}}},n={args:{problemInfo:{problemId:1223,title:"마법의 돌",tier:0},isTierHidden:!1,width:240,onHover:e()}},o={args:{problemInfo:{problemId:1237,title:"정ㅋ벅ㅋ",tier:31},isTierHidden:!1,width:240,onHover:e()}},s={args:{problemInfo:{problemId:29729,title:"가변 배열",tier:3},isTierHidden:!0,width:240,onHover:e()}},i={args:{problemInfo:{problemId:30403,title:"무지개 만들기",tier:4},isTierHidden:!1,width:240,onHover:e()}},t={args:{problemInfo:{problemId:2348,title:"내가 무릎을 꿇은 것은 추진력을 얻기 위함이었다",tier:10},isTierHidden:!1,width:240,onHover:e()}},a={args:{problemInfo:{problemId:1313,title:"합성소수",tier:12},isTierHidden:!1,width:240,onHover:e()}},d={args:{problemInfo:{problemId:27470,title:"멋진 부분집합",tier:16},isTierHidden:!1,width:240,onHover:e()}},m={args:{problemInfo:{problemId:31440,title:"히스토그램에서 가장 큰 직사각형과 쿼리 2",tier:23},isTierHidden:!1,width:240,onHover:e()}},l={args:{problemInfo:{problemId:31442,title:"좋은 수열",tier:26},isTierHidden:!1,width:240,onHover:e()}},r={args:{problemInfo:{problemId:15641,title:"SUPER SUPER BINARY SEARCH DELUXE 2.5: THE LEGEND OF THE GOLDEN MAZASSUMNIDA, EPISODE 2: THE MAZWAETL UNIVERSE, PART 2: THE PARALLEL UNIVERSE AND THE LOST MAZASSUMNIDA: GAME OF THE YEAR EDITION ",tier:31},isTierHidden:!1,width:240,onHover:e()}};var p,c,H;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 1223,
+      title: '마법의 돌',
+      tier: 0
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(H=(c=n.parameters)==null?void 0:c.docs)==null?void 0:H.source}}};var I,f,E;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 1237,
+      title: '정ㅋ벅ㅋ',
+      tier: 31
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(E=(f=o.parameters)==null?void 0:f.docs)==null?void 0:E.source}}};var b,T,u;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 29729,
+      title: '가변 배열',
+      tier: 3
+    },
+    isTierHidden: true,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(u=(T=s.parameters)==null?void 0:T.docs)==null?void 0:u.source}}};var S,g,A;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 30403,
+      title: '무지개 만들기',
+      tier: 4
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(A=(g=i.parameters)==null?void 0:g.docs)==null?void 0:A.source}}};var v,R,h;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 2348,
+      title: '내가 무릎을 꿇은 것은 추진력을 얻기 위함이었다',
+      tier: 10
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(h=(R=t.parameters)==null?void 0:R.docs)==null?void 0:h.source}}};var w,N,D;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 1313,
+      title: '합성소수',
+      tier: 12
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(D=(N=a.parameters)==null?void 0:N.docs)==null?void 0:D.source}}};var L,P,U;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 27470,
+      title: '멋진 부분집합',
+      tier: 16
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(U=(P=d.parameters)==null?void 0:P.docs)==null?void 0:U.source}}};var O,M,G;m.parameters={...m.parameters,docs:{...(O=m.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 31440,
+      title: '히스토그램에서 가장 큰 직사각형과 쿼리 2',
+      tier: 23
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(G=(M=m.parameters)==null?void 0:M.docs)==null?void 0:G.source}}};var Z,C,y;l.parameters={...l.parameters,docs:{...(Z=l.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 31442,
+      title: '좋은 수열',
+      tier: 26
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(y=(C=l.parameters)==null?void 0:C.docs)==null?void 0:y.source}}};var B,F,V,Y,x;r.parameters={...r.parameters,docs:{...(B=r.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    problemInfo: {
+      problemId: 15641,
+      title: 'SUPER SUPER BINARY SEARCH DELUXE 2.5: THE LEGEND OF THE GOLDEN MAZASSUMNIDA, EPISODE 2: THE MAZWAETL UNIVERSE, PART 2: THE PARALLEL UNIVERSE AND THE LOST MAZASSUMNIDA: GAME OF THE YEAR EDITION ',
+      tier: 31
+    },
+    isTierHidden: false,
+    width: 240,
+    onHover: fn()
+  }
+}`,...(V=(F=r.parameters)==null?void 0:F.docs)==null?void 0:V.source},description:{story:"문제 제목의 길이가 너무 길 경우 최대 2줄까지만 보여집니다.",...(x=(Y=r.parameters)==null?void 0:Y.docs)==null?void 0:x.description}}};const $=["Unrated","NotRatable","Hidden","Bronze","Silver","Gold","Platinum","Diamond","Ruby","LongTitle"];export{i as Bronze,m as Diamond,a as Gold,s as Hidden,r as LongTitle,o as NotRatable,d as Platinum,l as Ruby,t as Silver,n as Unrated,$ as __namedExportsOrder,Q as default};

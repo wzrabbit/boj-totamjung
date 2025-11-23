bb6943633cdc5a7d6f1b92fbed33ed524e8ceaa1
wzrabbit <curious.wzrabbit@gmail.com>
2025-11-23 20:24:24 +0900

    ✨ feat: "추첨 기록" 메뉴의 키보드 탐색을 방향키로 가능하게 하도록 변경

--- components/RandomDefenseHistoryMenu/RandomDefenseHistoryList/RandomDefenseHistoryList.tsx
+++ components/RandomDefenseHistoryMenu/RandomDefenseHistoryList/RandomDefenseHistoryList.tsx
@@ -1,5 +1,6 @@
 import * as S from './RandomDefenseHistoryList.styled';
 import RandomDefenseHistoryItem from './RandomDefenseHistoryItem';
+import useRovingFocus from '@/hooks/useRovingFocus';
 import type { RandomDefenseHistoryInfo } from '@/types/randomDefense';
 
 interface RandomDefenseHistoryList {
@@ -10,12 +11,18 @@ interface RandomDefenseHistoryList {
 
 const RandomDefenseHistoryList = (props: RandomDefenseHistoryList) => {
   const { items, isHidden, onDelete } = props;
+  const { getRovingProps } = useRovingFocus<HTMLAnchorElement>(items.length);
 
   return (
     <S.Container>
-      {items.map((item) => {
+      {items.map((item, index) => {
         const id = `${item.problemId}-${item.createdAt}`;
         const isCurrentTierHidden = ![0, 31].includes(item.tier) && isHidden;
+        const rovingProps = getRovingProps(index);
+        const modifiedRovingProps = {
+          ...rovingProps,
+          linkButtonRef: rovingProps.ref,
+        };
 
         return (
           <RandomDefenseHistoryItem
@@ -25,6 +32,7 @@ const RandomDefenseHistoryList = (props: RandomDefenseHistoryList) => {
               onDelete(id);
             }}
             {...item}
+            {...modifiedRovingProps}
           />
         );
       })}

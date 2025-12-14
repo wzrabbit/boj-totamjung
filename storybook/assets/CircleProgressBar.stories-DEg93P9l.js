6748f006c5db179f1b9426cf7632b176687586f8
wzrabbit <wzrabbit@users.noreply.github.com>
2025-12-14 13:59:08 +0000

    deploy: b1a1ddc5a62780e96c476ea6c8eaddda6103dd15

---
+++ storybook/assets/CircleProgressBar.stories-DEg93P9l.js
@@ -0,0 +1,17 @@
+import{C as d}from"./CircleProgressBar-DbK9OCO6.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";const u={title:"components/CircleProgressBar",component:d,argTypes:{size:{description:"프로그레스 바의 크기입니다."},progress:{description:"프로그레스 바가 얼마나 채워져 있는가를 의미합니다. **이 값은 0 이상 100 이하의 수여야 합니다.**",control:{type:"range",min:0,max:100}},color:{description:"프로그레스 바의, **채워져 있는 부분**의 색상입니다."},trackColor:{description:"프로그레스 바의, **채워져 있지 않은 부분**의 색상입니다."}},parameters:{docs:{description:{component:"`CircleProgressBar`는 진행 상황을 시각적으로 보여주는 컴포넌트입니다."}}}},o={args:{size:45,strokeWidth:5.625,progress:65,color:"#ffe091",trackColor:"#362b28"}},r={args:{size:45,strokeWidth:22.5,progress:65,color:"#ffe091",trackColor:"#362b28"}};var e,s,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    size: 45,
+    strokeWidth: 5.625,
+    progress: 65,
+    color: '#ffe091',
+    trackColor: '#362b28'
+  }
+}`,...(t=(s=o.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var a,c,i,n,p;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    size: 45,
+    strokeWidth: 22.5,
+    progress: 65,
+    color: '#ffe091',
+    trackColor: '#362b28'
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source},description:{story:"`strokeWidth` 값을 `size`의 절반으로 조절하면 꽉 찬 프로그레스 바를 만들 수 있습니다.",...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.description}}};const C=["Default","Filled"];export{o as Default,r as Filled,C as __namedExportsOrder,u as default};

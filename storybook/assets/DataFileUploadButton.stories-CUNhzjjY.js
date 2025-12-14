aa878aa9ed9ba9cce0fd04ed8404c3d96bb01f92
wzrabbit <wzrabbit@users.noreply.github.com>
2025-12-14 12:45:21 +0000

    deploy: 0d26041307dff3549cbc9643a1545aa814585f50

--- storybook/assets/DataFileUploadButton.stories-CUNhzjjY.js
+++
@@ -1,5 +0,0 @@
-import{D as r}from"./DataFileUploadButton-D46EKoSS.js";import{f as n}from"./index-BAc7KiQ7.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./index--qcDGAq6.js";import"./file-upload-mz0LOrA9.js";import"./styled-components.browser.esm-CIS6JKSM.js";const u={title:"components/OptionsDataManageMenu/DataFileUploadButton",component:r,argTypes:{onChange:{description:"사용자가 파일을 업로드한 경우 실행시킬 콜백 함수입니다."}},parameters:{docs:{description:{component:"`DataFileUploadButton`은 토탐정의 세이브파일을 업로드할 수 있도록 해 주는 버튼 컴포넌트입니다."}}}},o={args:{onChange:n()}};var t,a,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    onChange: fn()
-  }
-}`,...(e=(a=o.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const f=["Default"];export{o as Default,f as __namedExportsOrder,u as default};
--- storybook/assets/index-B5qaPpLJ.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./index--qcDGAq6.js";import{r as o}from"./index-B6ujFmsw.js";var t=o();const m=r(t);export{m as I,t as r};

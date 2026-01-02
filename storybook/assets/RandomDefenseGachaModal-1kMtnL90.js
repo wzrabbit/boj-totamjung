1d778dfc675e79f605fa7bb784e9c13fa8ad5054
wzrabbit <wzrabbit@users.noreply.github.com>
2026-01-02 14:49:08 +0000

    deploy: 6469caa595e63a5bb20a0bc040f2b3033d62f9d9

---
+++ storybook/assets/RandomDefenseGachaModal-1kMtnL90.js
@@ -0,0 +1,333 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{I as k}from"./IconButton-Cuc1o0yv.js";import{d as r,l as O,m as j}from"./styled-components.browser.esm-CIS6JKSM.js";import{M as se}from"./Modal-Cac1K4CQ.js";import{r as o}from"./index--qcDGAq6.js";import{S as ae}from"./copy-BcJBRJXh.js";import{S as le}from"./warning-BLusDAjJ.js";import{S as ce}from"./check-BIa-lIjI.js";import{S as de}from"./loading-Ckb1CsUZ.js";import{h as ue}from"./hidden-tier-badge-CukGKv-l.js";import{t as q}from"./theme-DpyODYgC.js";import{C as he}from"./CardBox-DVANTHr7.js";import{P as pe}from"./ProblemCardGrid-D0vZBhbh.js";import{C as I}from"./commands-BoKETmK5.js";import{i as Y,f as me,g as ge}from"./typeGuards-Bsa9ERJp.js";import{i as fe}from"./gachaOptionsValidator-CX_fe7zV.js";import{G as xe}from"./GachaModalNotification-DM5b6Hdn.js";const we=e=>o.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:32,height:32,viewBox:"0 0 16 16",...e},o.createElement("g",{fill:"currentColor"},o.createElement("path",{d:"M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"}),o.createElement("path",{fillRule:"evenodd",d:"M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182a.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"}))),ve=e=>o.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:32,height:32,viewBox:"0 0 24 24",...e},o.createElement("g",{fill:"none",stroke:"currentColor",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:1.5,color:"currentColor"},o.createElement("path",{d:"M13.5 5.5V2m0 10V9m0 13c6 0 7.5-4.51 7.5-10S19.5 2 13.5 2S6 6.51 6 12s1.5 10 7.5 10"}),o.createElement("path",{d:"M15 7c0-.466 0-.699-.076-.883a1 1 0 0 0-.541-.54c-.184-.077-.417-.077-.883-.077s-.699 0-.883.076a1 1 0 0 0-.54.541C12 6.301 12 6.534 12 7v.5c0 .466 0 .699.076.883a1 1 0 0 0 .541.54c.184.077.417.077.883.077s.699 0 .883-.076a1 1 0 0 0 .54-.541C15 8.199 15 7.966 15 7.5zm-9 5h15M5 3.167L4.619 2m-.59 3.567L3 6"}))),ye=e=>o.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:32,height:32,viewBox:"0 0 24 24",...e},o.createElement("g",{fill:"none",stroke:"currentColor",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:2},o.createElement("rect",{width:12,height:12,x:2,y:10,rx:2,ry:2}),o.createElement("path",{d:"m17.92 14l3.5-3.5a2.24 2.24 0 0 0 0-3l-5-4.92a2.24 2.24 0 0 0-3 0L10 6M6 18h.01M10 14h.01M15 6h.01M18 9h.01"}))),be=e=>o.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:32,height:32,viewBox:"0 0 24 24",...e},o.createElement("path",{fill:"currentColor",d:"M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.712T12 4t.713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062M6 20q-.825 0-1.412-.587T4 18v-2q0-.425.288-.712T5 15t.713.288T6 16v2h12v-2q0-.425.288-.712T19 15t.713.288T20 16v2q0 .825-.587 1.413T18 20z"})),Te=e=>o.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:32,height:32,viewBox:"0 0 24 24",...e},o.createElement("path",{fill:"currentColor",d:"M19 11.975q0-2.075-1.1-3.787t-2.95-2.563q-.375-.175-.55-.537t-.05-.738q.15-.4.538-.575t.787 0Q18.1 4.85 19.55 7.063T21 11.974t-1.45 4.913t-3.875 3.287q-.4.175-.788 0t-.537-.575q-.125-.375.05-.737t.55-.538q1.85-.85 2.95-2.562t1.1-3.788M7 15H4q-.425 0-.712-.288T3 14v-4q0-.425.288-.712T4 9h3l3.3-3.3q.475-.475 1.088-.213t.612.938v11.15q0 .675-.612.938T10.3 18.3zm9.5-3q0 1.05-.475 1.988t-1.25 1.537q-.25.15-.513.013T14 15.1V8.85q0-.3.263-.437t.512.012q.775.625 1.25 1.575t.475 2"})),Se=e=>o.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:32,height:32,viewBox:"0 0 24 24",...e},o.createElement("path",{fill:"currentColor",d:"M16.775 19.575q-.275.175-.55.325t-.575.275q-.375.175-.762 0t-.538-.575q-.15-.375.038-.737t.562-.538q.1-.05.188-.1t.187-.1L12 14.8v2.775q0 .675-.612.938T10.3 18.3L7 15H4q-.425 0-.712-.288T3 14v-4q0-.425.288-.712T4 9h2.2L2.1 4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l17 17q.275.275.275.7t-.275.7t-.7.275t-.7-.275zm2.225-7.6q0-2.075-1.1-3.787t-2.95-2.563q-.375-.175-.55-.537t-.05-.738q.15-.4.538-.575t.787 0Q18.1 4.85 19.55 7.05T21 11.975q0 .825-.15 1.638t-.425 1.562q-.2.55-.612.688t-.763.012t-.562-.45t-.013-.75q.275-.65.4-1.312T19 11.975m-4.225-3.55Q15.6 8.95 16.05 10t.45 2v.25q0 .125-.025.25q-.05.325-.35.425t-.55-.15L14.3 11.5q-.15-.15-.225-.337T14 10.775V8.85q0-.3.263-.437t.512.012M9.75 6.95Q9.6 6.8 9.6 6.6t.15-.35l.55-.55q.475-.475 1.087-.213t.613.938V8q0 .35-.3.475t-.55-.125z"})),je=""+new URL("tier-1-badge-CyfFUTea.png",import.meta.url).href,Ce=""+new URL("wood-background-BTxo82aE.png",import.meta.url).href,Me=j`
+  from {
+    transform: rotate(0deg);
+  }
+  
+  to {
+    transform: rotate(360deg);
+  }
+`,Ee=j`
+  0% {
+   opacity: 0.3;
+  }
+  
+  50% {
+    opacity: 1;
+  }
+
+  100% {
+   opacity: 0.3;
+  }
+`,Re=j`
+  0% {
+    transform: rotate(0deg);
+  }
+  
+  10% {
+    transform: rotate(-3deg);
+  }
+
+  20% {
+    transform: rotate(3deg);
+  }
+
+  30% {
+    transform: rotate(-3deg);
+  }
+
+  40% {
+    transform: rotate(3deg);
+  }
+
+  50% {
+    transform: rotate(0deg);
+  }
+`,Ae=j`
+  0% {
+    transform: scale(0.75);
+    opacity: 0;
+  }
+  
+  60% {
+    transform: scale(1.02);
+    opacity: 1;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+`,ke=j`
+  from {
+    pointer-events: none;
+    opacity: 0;
+  }
+
+  to {
+    pointer-events: auto;
+    opacity: 1;
+  }
+`,qe=r.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+
+  width: 800px;
+  height: 550px;
+  max-width: 100%;
+  max-height: 100%;
+
+  background-image: url(${Ce});
+  background-size: cover;
+
+  & > div {
+    flex-grow: 1;
+  }
+
+  &,
+  & * {
+    font-family: Pretendard;
+  }
+`,z=r.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 12px;
+  position: absolute;
+  top: 20px;
+
+  height: 35px;
+
+  opacity: 0.6;
+  transition: opacity 0.2s;
+  user-select: none;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  ${({$align:e})=>e==="left"?O`
+          left: 20px;
+        `:O`
+          right: 20px;
+        `};
+`,F=r.div`
+  display: inline-block;
+
+  color: ${({theme:e})=>e.color.LIGHT_GRAY};
+`,Q=O`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 35px;
+  height: 35px;
+
+  border: 2px solid ${({theme:e})=>e.color.LIGHT_GRAY};
+  border-radius: 50%;
+  background: transparent;
+
+  &:hover {
+    box-shadow: 0 0 10px ${({theme:e})=>e.color.LIGHT_GRAY};
+    transform: scale(1.1);
+  }
+
+  &:active {
+    transform: scale(1);
+  }
+
+  transition:
+    box-shadow,
+    transform 0.2s;
+`,Ie=r.button`
+  ${Q}
+
+  & > img {
+    height: 70%;
+
+    filter: ${({theme:e})=>e.filter.LIGHT_GRAY_FILTER};
+  }
+`,Le=r.button`
+  ${Q}
+
+  & > svg {
+    width: 100%;
+    height: 100%;
+
+    color: ${({theme:e})=>e.color.LIGHT_GRAY};
+  }
+`,Ge=r.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+`,De=r.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`,$e=r.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+
+  padding: 16px 32px;
+
+  max-width: 80%;
+
+  border-radius: 8px;
+  background-color: ${({theme:e})=>e.color.LIGHT_RED_TRANSPARENT};
+`,He=r.div`
+  display: flex;
+  justify-content: center;
+  column-gap: 8px;
+
+  width: 100%;
+  height: 100%;
+`,_e=r.div`
+  width: 30px;
+  height: 30px;
+
+  & > svg {
+    width: 100%;
+    height: 100%;
+
+    color: ${({theme:e})=>e.color.LIGHT_RED};
+  }
+`,Be=r.span`
+  font-size: 24px;
+  font-weight: 600;
+  color: ${({theme:e})=>e.color.LIGHT_RED};
+  text-align: center;
+`,Oe=r.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+`,V=r.li`
+  font-size: 16px;
+  color: ${({theme:e})=>e.color.WHITE};
+  word-break: break-all;
+`,B=r.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+
+  width: 100%;
+  height: 80px;
+
+  column-gap: 12px;
+`,Ne=r.div`
+  display: flex;
+  align-items: flex-end;
+
+  width: 100%;
+  height: 100%;
+`,Pe=r.div`
+  width: 20px;
+  height: 20px;
+
+  & > svg {
+    width: 100%;
+    height: 100%;
+
+    color: ${({theme:e})=>e.color.WHITE};
+  }
+
+  animation: ${Me} 3s linear infinite;
+`,W=r.div`
+  display: flex;
+  column-gap: 8px;
+`,ze=r.span`
+  font-size: 20px;
+  line-height: 20px;
+  font-weight: 500;
+
+  color: ${({theme:e})=>e.color.LIGHT_GRAY};
+
+  animation: ${Ee} 2s linear infinite;
+`,Fe=r.div`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  width: 100%;
+  height: 100%;
+`,Ve=r.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`,We=r.div`
+  width: 25px;
+  height: 25px;
+
+  & > svg {
+    width: 100%;
+    height: 100%;
+
+    color: ${({theme:e})=>e.color.PURE_WHITE};
+  }
+
+  animation: ${Re} 1.5s infinite;
+`,Ue=r.span`
+  font-size: 20px;
+  line-height: 25px;
+  font-weight: 500;
+
+  color: ${({theme:e})=>e.color.WHITE};
+`,Ye=r.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+`,Qe=r.div`
+  overflow: hidden;
+
+  width: 100%;
+  height: 100%;
+  padding: 60px 20px 20px 20px;
+
+  opacity: 0;
+  animation: ${Ae} cubic-bezier(0.165, 0.84, 0.44, 1) 0.7s 0.3s forwards;
+`,Je=r.div`
+  position: absolute;
+  left: 0;
+  bottom: 65px;
+
+  width: 100%;
+  height: 20px;
+`,Ke=r.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+
+  width: 100%;
+  height: 80px;
+
+  column-gap: 12px;
+
+  opacity: 0;
+  pointer-events: none;
+  animation: ${ke} 0.3s 1s forwards;
+`,Xe=e=>{if(!(Y(e)&&"success"in e&&typeof e.success=="boolean"))return!1;const{success:n}=e;if(n)return"problemInfos"in e&&et(e.problemInfos);if("statusCode"in e&&typeof e.statusCode!="number"||!("errorMessage"in e)||typeof e.errorMessage!="string")return!1;if(!("errorDescriptions"in e))return!0;const{errorDescriptions:s}=e;return typeof s=="string"||Array.isArray(s)&&s.every(i=>typeof i=="string")},Ze=e=>Y(e)&&"problemId"in e&&"title"in e&&"tier"in e&&typeof e.problemId=="number"&&typeof e.title=="string"&&me(e.tier),et=e=>Array.isArray(e)&&e.every(n=>Ze(n)),tt=["bronze","silver","gold","platinum","diamond","ruby"],ot=e=>{if(e.length===0)throw Error("프리뷰 카드 랭크 계산을 위해서는 최소 1문제의 정보가 제공되어야 합니다.");if(e.length===1){const l=e[0].tier;return[L(l)]}if(e.length===2){const l=e[0].tier,m=e[1].tier;return[L(l),L(m)]}const n={unrated:0,bronze:0,silver:0,gold:0,platinum:0,diamond:0,ruby:0};e.forEach(({tier:l})=>{n[L(l)]+=1});const s=Object.entries(n).sort((l,m)=>m[1]-l[1]);if(!rt(s))throw Error("프리뷰 카드 랭크 계산 중 데이터 불일치를 발견했습니다.");const[i,d]=s[0],a=s[1][0],u=s[2][0];return d===e.length?[i,i,i]:d*2>e.length?[i,i,a]:[i,a,u]},L=e=>e===0||e===31?"unrated":tt[Math.floor((e-1)/5)],rt=e=>e.every(([n])=>ge(n)),nt=e=>{const n=e.reduce((d,a)=>d+a.probability,0);if(Math.abs(n-1)>1e-6)throw new Error(`확률의 합은 1이어야 하는데, 실제 확률의 합이 ${n}였습니다.`);const s=Math.random();let i=0;for(const{choice:d,probability:a}of e)if(i+=a,s<=i)return d;throw new Error("잘못된 확률 정보를 사용하셨습니다. 다시 확인해 주세요.")},it=(e,n)=>Math.floor(Math.random()*(n-e+1))+e,st=""+new URL("card-slide-1-BWULmWDl.wav",import.meta.url).href,at=""+new URL("card-slide-2-DSpetA15.wav",import.meta.url).href,lt=""+new URL("card-slide-3-DsJdFER7.wav",import.meta.url).href,ct=""+new URL("card-slide-4-2Wc5wS26.wav",import.meta.url).href,dt=""+new URL("gacha-COr4DCSI.mp3",import.meta.url).href,ut=[st,at,lt,ct],ht="🎲",pt="✅",mt="📜",gt=(e,n)=>{const s=n.map(({problemId:i,title:d})=>`- ${i}번 - ${d} (https://acmicpc.net/problem/${i})`).join(`
+`);return`
+# 추첨 결과 ${ht}
+
+## 추첨 정보 ${pt}
+
+- 추첨 이름: ${e}
+- 문제 수: ${n.length}
+
+## 문제 목록 ${mt}
+${s}
+`.trim()},ft=[{choice:"black",probability:.24975},{choice:"red",probability:.24975},{choice:"green",probability:.24975},{choice:"blue",probability:.24975},{choice:"gold",probability:.001}],xt=ut.map(e=>new Audio(e)),wt=e=>{const{open:n,slot:s,problemCount:i}=e,[d,a]=o.useState("loading"),[u,l]=o.useState([]),[m,G]=o.useState("black"),[D,y]=o.useState(""),[b,h]=o.useState([]),[g,T]=o.useState(!1),[x,C]=o.useState(!0),[M,S]=o.useState(""),[$,w]=o.useState(!0),[H,E]=o.useState(!1),[R,_]=o.useState(!1),f=o.useRef(new Audio(dt)),A=u.length>0?ot(u):["unrated"],J=o.useCallback(async()=>{const c=await browser.runtime.sendMessage({command:I.GET_RANDOM_DEFENSE_RESULT,query:s.query,problemCount:i});if(!Xe(c)){y("API로부터 불러온 데이터에서 데이터 불일치가 발견되었습니다."),h("개발자에게 이 문제가 발생했음을 알려주세요."),a("error");return}if(!c.success){const{errorMessage:v,errorDescriptions:ie}=c;y(v),h(ie??[]),a("error");return}const p=c.problemInfos;l(p),a("ready")},[s,i]),K=o.useCallback(async()=>{const c=await browser.runtime.sendMessage({command:I.FETCH_GACHA_OPTIONS});if(!fe(c)){y("설정 데이터에서 불일치가 발견되었습니다."),h("개발자에게 이 문제가 발생했음을 알려주세요."),a("error");return}const{isTierHidden:p,isAudioMuted:v}=c;T(p),C(v),_(!0),f.current.muted=v},[]),N=()=>{a("loading"),E(!1),G(nt(ft)),J()},X=()=>{T(c=>!c)},Z=()=>{C(c=>{const p=!c;return f.current.muted=p,p})},ee=()=>{x||xt[it(0,3)].play()},te=()=>{P(),f.current.play()},P=()=>{f.current.pause(),f.current.currentTime=0},oe=()=>{navigator.clipboard.writeText(gt(s.title,u)),S("문제 목록을 클립보드에 복사했어요!"),w(!1),setTimeout(()=>w(!0))},re=()=>{a("showingResult"),S("")},ne=()=>{const c=new Date().toISOString(),p=u.map(v=>({...v,createdAt:c})).reverse();browser.runtime.sendMessage({command:I.ADD_RANDOM_DEFENSE_HISTORY_INFOS,randomDefenseHistoryInfos:p}),E(!0),S("문제 목록을 추첨 기록에 저장했어요!"),w(!1),setTimeout(()=>w(!0))};return o.useEffect(()=>{n&&N()},[n,s,i]),o.useEffect(()=>{K()},[]),o.useEffect(()=>{R&&browser.runtime.sendMessage({command:I.SAVE_GACHA_OPTIONS,isTierHidden:g,isAudioMuted:x})},[R,g,x]),{gachaStatus:d,problemInfos:u,cardBoxColor:m,previewCardRanks:A,errorMessage:D,errorDescriptions:b,isTierHidden:g,isAudioMuted:x,notificationMessage:M,shouldNotificationFadeOut:$,isSavedToHistory:H,restartGacha:N,toggleIsTierHidden:X,toggleIsAudioMuted:Z,playCardSlideAudio:ee,playGachaAudio:te,stopGachaAudio:P,copyProblemInfosMarkdownToClipboard:oe,showResultScreenAndResetNotificationMessage:re,saveGachaResultToStorage:ne}},U=e=>{const{open:n,theme:s="totamjung",slot:i,problemCount:d,portalTarget:a,onClose:u}=e,{gachaStatus:l,problemInfos:m,cardBoxColor:G,previewCardRanks:D,errorMessage:y,errorDescriptions:b,isTierHidden:h,isAudioMuted:g,isSavedToHistory:T,notificationMessage:x,shouldNotificationFadeOut:C,restartGacha:M,toggleIsTierHidden:S,toggleIsAudioMuted:$,playCardSlideAudio:w,playGachaAudio:H,stopGachaAudio:E,copyProblemInfosMarkdownToClipboard:R,showResultScreenAndResetNotificationMessage:_,saveGachaResultToStorage:f}=wt({open:n,slot:i,problemCount:d});return t.jsx(se,{title:"즉석 추첨",open:n,theme:s,padding:"0",closeOnBackdropClick:!1,portalTarget:a,onClose:()=>{E(),u()},children:t.jsxs(qe,{children:[l==="loading"&&t.jsx(Ne,{children:t.jsx(B,{children:t.jsxs(W,{children:[t.jsx(Pe,{children:t.jsx(de,{})}),t.jsx(ze,{children:"잠시만 기다려주세요..."})]})})}),l==="ready"&&t.jsxs(Fe,{children:[t.jsx(Ve,{children:t.jsx(he,{color:G,isTierHidden:h,cardRanks:D,onFirstClick:H,onOpenAnimationEnd:_})}),t.jsx(B,{children:t.jsxs(W,{children:[t.jsx(We,{children:t.jsx(ve,{})}),t.jsx(Ue,{children:"카드 상자를 클릭하면 결과를 확인할 수 있습니다"})]})})]}),l==="error"&&t.jsxs(Ge,{children:[t.jsx(De,{children:t.jsxs($e,{children:[t.jsxs(He,{children:[t.jsx(_e,{children:t.jsx(le,{})}),t.jsx(Be,{children:y})]}),t.jsx(Oe,{children:typeof b=="string"?t.jsx(V,{children:b}):b.map(A=>t.jsx(V,{children:A},A))})]})}),t.jsx(B,{children:t.jsx(k,{type:"button",name:"다시 시도하기",size:"large",color:q.color.LIGHT_RED,iconSrc:t.jsx(we,{}),disabled:!1,onClick:M})})]}),l==="showingResult"&&t.jsxs(Ye,{children:[t.jsx(Qe,{children:t.jsx(pe,{problemInfos:m,onCardHover:w,isTierHidden:h})}),t.jsx(Je,{children:t.jsx(xe,{shouldFadeOut:C,children:x})}),t.jsxs(Ke,{children:[t.jsx(k,{type:"button",name:"문제 목록 복사",size:"large",color:q.color.LIGHT_GRAY,iconSrc:t.jsx(ae,{}),disabled:!1,onClick:R}),t.jsx(k,{type:"button",name:"추첨 기록 저장",size:"large",color:q.color.LEMON,iconSrc:T?t.jsx(ce,{}):t.jsx(be,{}),disabled:T,onClick:f}),t.jsx(k,{type:"button",name:"다시 추첨하기!",size:"large",color:q.color.ORANGE,iconSrc:t.jsx(ye,{}),disabled:!1,onClick:M})]})]}),t.jsxs(z,{$align:"left",children:[t.jsx(Ie,{onClick:S,"aria-label":h?"문제 난이도 보이기":"문제 난이도 감추기",children:h?t.jsx("img",{src:ue,alt:"",draggable:!1}):t.jsx("img",{src:je,alt:"",draggable:!1})}),t.jsx(F,{children:`티어 숨기기: ${h?"ON":"OFF"}`})]}),t.jsxs(z,{$align:"right",children:[t.jsx(F,{children:`효과음: ${g?"OFF":"ON"}`}),t.jsx(Le,{onClick:$,"aria-label":g?"효과음 켜기":"효과음 끄기",children:g?t.jsx(Se,{}):t.jsx(Te,{})})]})]})})};try{U.displayName="RandomDefenseGachaModal",U.__docgenInfo={description:"",displayName:"RandomDefenseGachaModal",props:{open:{defaultValue:null,description:"",name:"open",required:!0,type:{name:"boolean"}},theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"enum",value:[{value:'"none"'},{value:'"totamjung"'},{value:'"bojExtendedDark"'},{value:'"bojExtendedRigel"'},{value:'"solvedAcLight"'},{value:'"solvedAcDark"'},{value:'"solvedAcBlack"'}]}},slot:{defaultValue:null,description:"",name:"slot",required:!0,type:{name:"FilledSlot"}},problemCount:{defaultValue:null,description:"",name:"problemCount",required:!0,type:{name:"number"}},portalTarget:{defaultValue:null,description:"",name:"portalTarget",required:!1,type:{name:"HTMLElement | null"}},onClose:{defaultValue:null,description:"",name:"onClose",required:!0,type:{name:"() => void"}}}}}catch{}export{U as R,ye as S,Xe as i};

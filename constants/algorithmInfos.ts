89848ebafe7068b517e8e103240c2b03491176a4
wzrabbit <curious.wzrabbit@gmail.com>
2026-02-01 23:33:11 +0900

    ✨ feat: '키타마사' 알고리즘명을 '다항식을 이용한 선형점화식 계산' 으로 변경
    - 기존 이름은 `alias`에 추가

--- constants/algorithmInfos.ts
+++ constants/algorithmInfos.ts
@@ -1018,10 +1018,10 @@ export const ALGORITHM_INFOS: Readonly<AlgorithmInfo[]> = [
   },
   {
     id: 150,
-    name: '키타마사',
+    name: '다항식을 이용한 선형점화식 계산',
     englishName: 'Kitamasa',
     tag: 'kitamasa',
-    alias: [],
+    alias: ['키타마사'],
   },
   {
     id: 151,

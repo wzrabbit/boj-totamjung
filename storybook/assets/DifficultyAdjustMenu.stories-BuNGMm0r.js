e067333c3899c5e38f9c93551536d5abd88e5fbd
wzrabbit <wzrabbit@users.noreply.github.com>
2026-02-01 17:55:42 +0000

    deploy: 609061a0cab12b045af9bd1b074f47d882c14ce3

---
+++ storybook/assets/DifficultyAdjustMenu.stories-BuNGMm0r.js
@@ -0,0 +1,7 @@
+import{D as n}from"./DifficultyAdjustMenu-0XJUallX.js";import{f as i}from"./index-BAc7KiQ7.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";import"./TierSlider-B-Wa-Cg5.js";import"./typeGuards-Bsa9ERJp.js";import"./TierRange-BhJJoGeE.js";import"./index-CElCL35T.js";import"./TierPresetButtonList-BMscwYV7.js";import"./TierPresetButton-BLwKsjcE.js";const y={title:"components/RandomDefenseCreateMenu/DifficultyAdjustMenu",component:n,argTypes:{startTier:{description:"난이도 범위에서의 **시작 티어**입니다."},endTier:{description:"난이도 범위에서의 **끝 티어**입니다."},onChange:{description:"난이도 범위가 변경되어야 할 경우 실행시킬 콜백 함수입니다."}},parameters:{docs:{description:{component:"`DifficultyAdjustMenu`는 무작위 추첨에서 난이도의 범위를 설정할 수 있는 메뉴형 컴포넌트입니다."}}}},e={args:{startTier:1,endTier:30,onChange:i()}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    startTier: 1,
+    endTier: 30,
+    onChange: fn()
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,y as default};

b5b271d21769649840be5fce702600bd2857477a
wzrabbit <wzrabbit@users.noreply.github.com>
2026-02-02 07:11:04 +0000

    deploy: d052dbb1efcf54ecb5c1600c1efc0a42509dfda0

--- storybook/assets/HotkeySwitcher.stories-WoCiYSZc.js
+++
@@ -1,7 +0,0 @@
-import{H as n}from"./HotkeySwitcher-BR8w-1p1.js";import{f as s}from"./index-BAc7KiQ7.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./styled-components.browser.esm-CIS6JKSM.js";import"./index--qcDGAq6.js";import"./Text-CCW1h-Yj.js";import"./toPx-DeOSKary.js";const k={title:"components/QuickSlotsMenu/HotkeySwitcher",component:n,argTypes:{},parameters:{docs:{description:{component:"`HotkeySwitcher`는 무작위 추첨을 시작하기 위한 단축키를 스위칭하는 기능을 제공하기 위한 컴포넌트입니다. `Alt/Option`과 `F2`를 번갈아 스위칭하는 것이 가능합니다. `Alt/Option`의 경우 사용자의 운영체제에 따라 `Alt` 또는 `Option` 중 하나가 표시됩니다."}}}},t={args:{selectedSlotNo:3,hotkey:"Alt",onClick:s()}};var o,e,r;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    selectedSlotNo: 3,
-    hotkey: 'Alt',
-    onClick: fn()
-  }
-}`,...(r=(e=t.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,k as default};
